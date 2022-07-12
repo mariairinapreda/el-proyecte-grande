c35ff023bbb7a9d962db29cb4bd9f7de96f3428e
Loredana27 <loredanastefania1227@gmail.com>
2022-07-12 16:05:19 +0300

    implement DisplayDAO AND DisplayMemory AND DisplayService

---
+++ server/src/main/java/com/codecool/brightxchange/Service/modelDAO/Implementation/DisplayMemory.java
@@ -0,0 +1,8 @@
+package com.codecool.brightxchange.Service.modelDAO.Implementation;
+
+import com.codecool.brightxchange.Service.modelDAO.DAO.DisplayDAO;
+import org.springframework.stereotype.Repository;
+
+@Repository("DisplayMemory")
+public class DisplayMemory implements DisplayDAO {
+}
