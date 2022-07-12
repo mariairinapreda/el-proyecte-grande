792394eb76e8aac148638dfbc0d599c63dfa42ea
Loredana27 <loredanastefania1227@gmail.com>
2022-07-12 16:03:08 +0300

    implement CaseTypeDAO AND CaseTypeMemory AND CaseTypeService

---
+++ server/src/main/java/com/codecool/brightxchange/Service/modelDAO/Implementation/CaseTypeMemory.java
@@ -0,0 +1,8 @@
+package com.codecool.brightxchange.Service.modelDAO.Implementation;
+
+import com.codecool.brightxchange.Service.modelDAO.DAO.CaseTypeDAO;
+import org.springframework.stereotype.Repository;
+
+@Repository("CaseTypeMemory")
+public class CaseTypeMemory implements CaseTypeDAO {
+}
