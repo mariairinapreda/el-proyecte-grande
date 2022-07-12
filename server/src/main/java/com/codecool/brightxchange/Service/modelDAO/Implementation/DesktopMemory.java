bf310bd01f4a38cab0d20b2986df0686b1d2c79f
Loredana27 <loredanastefania1227@gmail.com>
2022-07-12 11:29:18 +0300

    implement DesktopDAO AND DesktopMemory AND DesktopService

---
+++ server/src/main/java/com/codecool/brightxchange/Service/modelDAO/Implementation/DesktopMemory.java
@@ -0,0 +1,8 @@
+package com.codecool.brightxchange.Service.modelDAO.Implementation;
+
+import com.codecool.brightxchange.Service.modelDAO.DAO.DesktopDAO;
+import org.springframework.stereotype.Repository;
+
+@Repository("DesktopMemory")
+public class DesktopMemory implements DesktopDAO {
+}
