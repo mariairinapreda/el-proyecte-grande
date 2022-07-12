0bfff28f198565d22ca03566737ec3bc47c3b120
Sidor Andrei <sidor.marian.andrei3001@gmail.com>
2022-07-12 09:52:52 +0300

    add SupplierMemory

---
+++ server/src/main/java/com/codecool/brightxchange/Service/modelDAO/Implementation/SupplierMemory.java
@@ -0,0 +1,8 @@
+package com.codecool.brightxchange.Service.modelDAO.Implementation;
+
+import com.codecool.brightxchange.Service.modelDAO.DAO.SupplierDAO;
+import org.springframework.stereotype.Repository;
+
+@Repository("SupplierMemory")
+public class SupplierMemory implements SupplierDAO {
+}
