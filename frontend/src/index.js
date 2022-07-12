fc066a423b78319b3fa4f2be783213a994d3d86e
SidorAndrei <sidor.marian.andrei3001@gmail.com>
2022-07-13 01:33:54 +0300

    implement react routing

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {
     BrowserRouter,
     Routes,
     Route,
 } from "react-router-dom";
-import navigation from "./components/navigation";
 import footer from "./components/footer";
 import landingPage from "./componentPages/LandingPage";
 
@@ -16,15 +14,18 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
 root.render(
-  // <React.StrictMode>
+  <React.StrictMode>
       <BrowserRouter>
           <Routes>
               <Route path="/" element={landingPage}/>
               <Route path="/add-case" element={footer}/>
+              <Route path="/cases" element={}>
+                  <Route path=":caseId" element={}></Route>
+              </Route>
           </Routes>
       </BrowserRouter>
 
-  // </React.StrictMode>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
