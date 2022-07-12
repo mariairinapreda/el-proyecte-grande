b937d6037470d11975877600b0612bc9c7792a58
Loredana27 <loredanastefania1227@gmail.com>
2022-07-12 16:03:43 +0300

    implement ChipsetProducerDAO AND ChipsetProducerMemory AND ChipsetProducerService

---
+++ server/src/main/java/com/codecool/brightxchange/Service/ChipsetProducerService.java
@@ -0,0 +1,14 @@
+package com.codecool.brightxchange.Service;
+
+import com.codecool.brightxchange.Service.modelDAO.DAO.ChipsetProducerDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+@Service("ChipsetProducerService")
+public class ChipsetProducerService implements ChipsetProducerDAO {
+    @Autowired
+    @Qualifier("ChipsetProducerMemory")
+    private ChipsetProducerDAO chipsetProducerDAO;
+}
