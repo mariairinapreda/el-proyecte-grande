cc3481f081c305324a3e0eaafa2ac37056d3d1ba
mariairinapreda <mariairina.predaeu@gmail.com>
2022-07-12 10:02:56 +0300

    Added BaseProduct

--- server/src/main/java/com/codecool/brightxchange/model/BaseProduct.java
+++ server/src/main/java/com/codecool/brightxchange/model/BaseProduct.java
@@ -1,14 +1,17 @@
 package com.codecool.brightxchange.model;
 
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
 abstract class BaseProduct {
     protected int id;
-    protected int price;
+    protected float price;
     protected PriceCurrency currency;
     protected int quantity;
     protected Supplier supplier;
 
 
-    public BaseProduct(int price, PriceCurrency currency, int quantity, Supplier supplier) {
+    public BaseProduct(int id,float price, PriceCurrency currency, int quantity, Supplier supplier) {
         this.id = id;
         this.price = price;
         this.currency = currency;
@@ -16,35 +19,6 @@ abstract class BaseProduct {
         this.supplier = supplier;
     }
 
-    public int getPrice() {
-        return price;
-    }
 
-    public void setPrice(int price) {
-        this.price = price;
-    }
 
-    public PriceCurrency getCurrency() {
-        return currency;
-    }
-
-    public void setCurrency(PriceCurrency currency) {
-        this.currency = currency;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
-    public Supplier getSupplier() {
-        return supplier;
-    }
-
-    public void setSupplier(Supplier supplier) {
-        this.supplier = supplier;
-    }
 }
