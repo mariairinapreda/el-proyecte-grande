55d48dcd7c46e44b914fec5c795e2408964f67f2
Loredana27 <loredanastefania1227@gmail.com>
2022-07-12 16:04:54 +0300

    implement CoolingSystemDAO AND CoolingSystemMemory AND CoolingSystemService

---
+++ server/src/main/java/com/codecool/brightxchange/Service/CoolingSystemService.java
@@ -0,0 +1,14 @@
+package com.codecool.brightxchange.Service;
+
+import com.codecool.brightxchange.Service.modelDAO.DAO.CoolingSystemDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+@Service("CoolingSystemService")
+public class CoolingSystemService implements CoolingSystemDAO {
+    @Autowired
+    @Qualifier("CoolingSystemMemory")
+    private CoolingSystemDAO coolingSystemDAO;
+}
---
+++ server/src/main/java/com/codecool/brightxchange/Service/modelDAO/Implementation/CoolingSystemMemory.java
@@ -0,0 +1,8 @@
+package com.codecool.brightxchange.Service.modelDAO.Implementation;
+
+import com.codecool.brightxchange.Service.modelDAO.DAO.CoolingSystemDAO;
+import org.springframework.stereotype.Repository;
+
+@Repository("CoolingSystemMemory")
+public class CoolingSystemMemory implements CoolingSystemDAO {
+}
