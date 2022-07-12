3053e3ca2b9a135ad42a311b89d6fcdddad3b802
mariairinapreda <mariairina.predaeu@gmail.com>
2022-07-12 12:07:17 +0300

    added baseProducer class

---
+++ server/src/main/java/com/codecool/brightxchange/model/BaseProducer.java
@@ -0,0 +1,14 @@
+package com.codecool.brightxchange.model;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+abstract class BaseProducer {
+    protected int id;
+    protected String producer;
+
+    public BaseProducer(int id, String producer) {
+        this.id = id;
+        this.producer = producer;
+    }
+}
