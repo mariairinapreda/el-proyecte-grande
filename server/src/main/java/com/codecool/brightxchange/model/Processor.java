40719e3e4d58e03fccdeffec2c053d25609a83a5
Loredana27 <loredanastefania1227@gmail.com>
2022-07-12 14:03:59 +0300

    implement Processor

---
+++ server/src/main/java/com/codecool/brightxchange/model/Processor.java
@@ -0,0 +1,40 @@
+package com.codecool.brightxchange.model;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+public class Processor extends BaseProduct{
+    private int id;
+
+    private ProcessorProducer producer;
+
+    private int socket;
+
+    private String core;
+
+    private int numberOfCores;
+
+    private int threads;
+
+    private int frequency;
+
+    private int frequencyTurboMax;
+
+    private int productionFactory;
+
+    public Processor(int id, ProcessorProducer producer, int socket, String core, int numberOfCores, int threads, int frequency, int frequencyTurboMax, int productionFactory) {
+        this.id = id;
+        this.producer = producer;
+        this.socket = socket;
+        this.core = core;
+        this.numberOfCores = numberOfCores;
+        this.threads = threads;
+        this.frequency = frequency;
+        this.frequencyTurboMax = frequencyTurboMax;
+        this.productionFactory = productionFactory;
+    }
+}
