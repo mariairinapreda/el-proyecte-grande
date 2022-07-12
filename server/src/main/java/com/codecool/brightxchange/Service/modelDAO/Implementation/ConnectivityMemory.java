117595b5b4cc938500c4e4af600602b037f3e2fd
Loredana27 <loredanastefania1227@gmail.com>
2022-07-12 16:04:26 +0300

    implement ConnectivityDAO AND ConnectivityMemory AND ConnectivityService

---
+++ server/src/main/java/com/codecool/brightxchange/Service/modelDAO/Implementation/ConnectivityMemory.java
@@ -0,0 +1,8 @@
+package com.codecool.brightxchange.Service.modelDAO.Implementation;
+
+import com.codecool.brightxchange.Service.modelDAO.DAO.ConnectivityDAO;
+import org.springframework.stereotype.Repository;
+
+@Repository("ConnectivityMemory")
+public class ConnectivityMemory implements ConnectivityDAO {
+}
