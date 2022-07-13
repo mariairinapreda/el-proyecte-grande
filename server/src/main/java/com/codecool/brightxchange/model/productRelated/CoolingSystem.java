729316ef3582e10e53de57e67d6511312ef46102
Sidor Andrei <sidor.marian.andrei3001@gmail.com>
2022-07-13 15:07:49 +0300

    implement entity for CoolingSystem

--- server/src/main/java/com/codecool/brightxchange/model/productRelated/CoolingSystem.java
+++ server/src/main/java/com/codecool/brightxchange/model/productRelated/CoolingSystem.java
@@ -5,16 +5,43 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
 @Data
 @Builder
 @NoArgsConstructor
+@Table(
+        name = "cooling_system",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "cooling_system_unique", columnNames = "type")
+        }
+)
+@Entity(name = "CoolingSystem")
 public class CoolingSystem {
-    @JsonProperty("coolingSystemId")
-    private int id;
-    @JsonProperty("coolingSystemType")
+    @Id
+    @SequenceGenerator(
+            name = "cooling_system_id_sequence",
+            sequenceName = "cooling_system_id_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "cooling_system_id_sequence"
+    )
+    @Column(
+            name = "id",
+            updatable = false
+    )
+    @JsonProperty("id")
+    private Long id;
+    @Column(
+            name = "type",
+            columnDefinition = "TEXT"
+    )
+    @JsonProperty("type")
     private String type;
 
-    public CoolingSystem(int id, String type) {
+    public CoolingSystem(Long id, String type) {
         this.id = id;
         this.type = type;
     }
