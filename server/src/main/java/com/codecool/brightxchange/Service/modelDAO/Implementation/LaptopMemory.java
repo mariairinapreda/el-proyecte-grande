c3dce6ae1b5d43f2e190afc58583e74708bedb83
Loredana27 <loredanastefania1227@gmail.com>
2022-07-13 09:35:30 +0300

    implement LaptopDAO AND LaptopMemory AND LaptopService

---
+++ server/src/main/java/com/codecool/brightxchange/Service/modelDAO/Implementation/LaptopMemory.java
@@ -0,0 +1,8 @@
+package com.codecool.brightxchange.Service.modelDAO.Implementation;
+
+import com.codecool.brightxchange.Service.modelDAO.DAO.LaptopDAO;
+import org.springframework.stereotype.Repository;
+
+@Repository("LaptopMemory")
+public class LaptopMemory implements LaptopDAO {
+}
