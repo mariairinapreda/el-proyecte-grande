7f64b0f4fa8050e7e26e081cd4ee7930c39277aa
Sidor Andrei <sidor.marian.andrei3001@gmail.com>
2022-07-13 14:51:01 +0300

    implement entity for PowerSupplyType

--- server/src/main/java/com/codecool/brightxchange/model/productTypes/PowerSupplyType.java
+++ server/src/main/java/com/codecool/brightxchange/model/productTypes/PowerSupplyType.java
@@ -1,13 +1,49 @@
 package com.codecool.brightxchange.model.productTypes;
 
-import com.codecool.brightxchange.model.productTypes.BaseType;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
 @Data
 @Builder
 @NoArgsConstructor
-public class PowerSupplyType extends BaseType {
+@Entity(name = "PowerSupplyType")
+@Table(name = "power_supply_type",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "power_supply_type_name_unique", columnNames = "name")
+        }
+)
+public class PowerSupplyType{
+    @Id
+    @SequenceGenerator(
+            name = "case_type_id_sequence",
+            sequenceName = "case_type_id_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "case_type_id_sequence"
+    )
+    @Column(
+            name = "id",
+            updatable = false
+    )
+    @JsonProperty("id")
+    private Long id;
+    @Column(
+            name = "name",
+            columnDefinition = "TEXT"
+    )
+    @JsonProperty("name")
+    private String name;
+
+    public PowerSupplyType(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
 
 }
