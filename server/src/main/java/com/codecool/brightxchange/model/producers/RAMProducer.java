6071fa96bc881f26676ad47ce1528beea603df35
Loredana27 <loredanastefania1227@gmail.com>
2022-07-13 15:08:23 +0300

    implement entity for RAMProducer

--- server/src/main/java/com/codecool/brightxchange/model/producers/RAMProducer.java
+++ server/src/main/java/com/codecool/brightxchange/model/producers/RAMProducer.java
@@ -1,12 +1,49 @@
 package com.codecool.brightxchange.model.producers;
 
-import com.codecool.brightxchange.model.producers.BaseProducer;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
 @Data
 @NoArgsConstructor
 @Builder
-public class RAMProducer extends BaseProducer {
+@Entity(name = "RAMProducer")
+@Table(
+        name = "ram_producer",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "name_unique", columnNames = "name")
+        }
+)
+public class RAMProducer {
+    @JsonProperty("id")
+    @Id
+    @SequenceGenerator(
+            name = "ram_producer_sequence",
+            sequenceName = "ram_producer_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "ram_producer_sequence"
+    )
+    @Column(
+            name = "id",
+            updatable = false
+    )
+    private Long id;
+
+    @JsonProperty("name")
+    @Column(
+            name = "name",
+            columnDefinition = "TEXT"
+    )
+    private String name;
+
+    public RAMProducer(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 }
