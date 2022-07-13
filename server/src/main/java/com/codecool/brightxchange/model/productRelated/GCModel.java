3c216d3a4f64933983713a4f9f1154383d25be5f
Loredana27 <loredanastefania1227@gmail.com>
2022-07-13 15:45:24 +0300

    implement entity for GCModel

--- server/src/main/java/com/codecool/brightxchange/model/productRelated/GCModel.java
+++ server/src/main/java/com/codecool/brightxchange/model/productRelated/GCModel.java
@@ -5,16 +5,39 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
 @Data
 @NoArgsConstructor
 @Builder
+@Entity(name = "GCModel")
+@Table(
+        name = "gc_model",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "name_unique", columnNames = "name")
+        }
+)
 public class GCModel {
     @JsonProperty("gcModelId")
-    private int id;
+    @Id
+    @SequenceGenerator(
+            name = "gc_model_sequence",
+            sequenceName = "gc_model_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "gc_model_sequence"
+    )
+    private Long id;
     @JsonProperty("gcModelName")
+    @Column(
+            name = "name",
+            columnDefinition = "TEXT"
+    )
     private String name;
 
-    public GCModel(int id, String name) {
+    public GCModel(Long id, String name) {
         this.id = id;
         this.name = name;
     }
