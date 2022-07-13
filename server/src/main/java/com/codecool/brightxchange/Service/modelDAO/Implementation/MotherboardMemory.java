c8f7229f2630000f9fb2727c75fad43530b3d6a9
Loredana27 <loredanastefania1227@gmail.com>
2022-07-13 09:46:31 +0300

    implement MotherboardMemory AND  MotherboardService AND  MotherboardDAO

---
+++ server/src/main/java/com/codecool/brightxchange/Service/modelDAO/Implementation/MotherboardMemory.java
@@ -0,0 +1,8 @@
+package com.codecool.brightxchange.Service.modelDAO.Implementation;
+
+import com.codecool.brightxchange.Service.modelDAO.DAO.MotherboardDAO;
+import org.springframework.stereotype.Repository;
+
+@Repository("MotherboardMemory")
+public class MotherboardMemory implements MotherboardDAO {
+}
