02f4a0e3428073882b952e404e5118664dc82295
Loredana27 <loredanastefania1227@gmail.com>
2022-07-13 15:45:41 +0300

    implement entity for GraphicsCardFamily

--- server/src/main/java/com/codecool/brightxchange/model/productRelated/GraphicsCardFamily.java
+++ server/src/main/java/com/codecool/brightxchange/model/productRelated/GraphicsCardFamily.java
@@ -5,16 +5,39 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
 @Data
 @Builder
 @NoArgsConstructor
+@Entity(name = "GraphicsCardFamily")
+@Table(
+        name = "graphics_card_family",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "name_unique", columnNames = "name")
+        }
+        )
 public class GraphicsCardFamily {
     @JsonProperty("graphicsCardFamilyId")
-    private int id;
+    @Id
+    @SequenceGenerator(
+            name = "graphics_card_family_sequence",
+            sequenceName = "graphics_card_family_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "graphics_card_family_sequence"
+    )
+    private Long id;
     @JsonProperty("graphicsCardFamilyName")
+    @Column(
+            name = "name",
+            columnDefinition = "TEXT"
+    )
     private String name;
 
-    public GraphicsCardFamily(int id, String name) {
+    public GraphicsCardFamily(Long id, String name) {
         this.id = id;
         this.name = name;
     }
