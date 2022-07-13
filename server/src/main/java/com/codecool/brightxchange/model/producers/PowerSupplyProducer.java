52b3cb1acfcf839750e57eb16dd8f7d9115ed586
Loredana27 <loredanastefania1227@gmail.com>
2022-07-13 15:08:06 +0300

    implement entity for ProcessorProducer

--- server/src/main/java/com/codecool/brightxchange/model/producers/PowerSupplyProducer.java
+++ server/src/main/java/com/codecool/brightxchange/model/producers/PowerSupplyProducer.java
@@ -1,12 +1,49 @@
 package com.codecool.brightxchange.model.producers;
 
-import com.codecool.brightxchange.model.producers.BaseProducer;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
 @Data
 @Builder
 @NoArgsConstructor
-public class PowerSupplyProducer extends BaseProducer {
+@Entity(name = "PowerSupplyProducer")
+@Table(
+        name = "power_supply_producer",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "name_unique", columnNames = "name")
+        }
+)
+public class PowerSupplyProducer {
+    @JsonProperty("id")
+    @Id
+    @SequenceGenerator(
+            name = "power_supply_producer_sequence",
+            sequenceName = "power_supply_producer_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "power_supply_producer_sequence"
+    )
+    @Column(
+            name = "id",
+            updatable = false
+    )
+    private Long id;
+
+    @JsonProperty("name")
+    @Column(
+            name = "name",
+            columnDefinition = "TEXT"
+    )
+    private String name;
+
+    public PowerSupplyProducer(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 }
