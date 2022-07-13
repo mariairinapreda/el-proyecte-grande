8cbd6718442ceab2eb02a787eafba2e868c10267
Loredana27 <loredanastefania1227@gmail.com>
2022-07-13 09:44:17 +0300

    implement VentProducerMemory AND VentProducerService AND VentProducerDAO

---
+++ server/src/main/java/com/codecool/brightxchange/Service/VentProducerService.java
@@ -0,0 +1,15 @@
+package com.codecool.brightxchange.Service;
+
+import com.codecool.brightxchange.Service.modelDAO.DAO.VentProducerDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+@Service("VentProducerService")
+public class VentProducerService {
+    @Autowired
+    @Qualifier("VentProducerMemory")
+    private VentProducerDAO ventProducerDAO;
+
+}
---
+++ server/src/main/java/com/codecool/brightxchange/Service/modelDAO/Implementation/VentProducerMemory.java
@@ -0,0 +1,8 @@
+package com.codecool.brightxchange.Service.modelDAO.Implementation;
+
+import com.codecool.brightxchange.Service.modelDAO.DAO.VentProducerDAO;
+import org.springframework.stereotype.Repository;
+
+@Repository("VentProducerMemory")
+public class VentProducerMemory implements VentProducerDAO {
+}
