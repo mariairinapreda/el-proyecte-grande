9b934db8189f36fdc219d6726bae8a716589919e
Loredana27 <loredanastefania1227@gmail.com>
2022-07-13 15:07:22 +0300

    implement entity for LaptopProducer

--- server/src/main/java/com/codecool/brightxchange/model/producers/LaptopProducer.java
+++ server/src/main/java/com/codecool/brightxchange/model/producers/LaptopProducer.java
@@ -1,12 +1,49 @@
 package com.codecool.brightxchange.model.producers;
 
-import com.codecool.brightxchange.model.producers.BaseProducer;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
 @Data
 @Builder
 @NoArgsConstructor
-public class LaptopProducer extends BaseProducer {
+@Entity(name = "LaptopProducer")
+@Table(
+        name = "laptop_producer",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "name_unique", columnNames = "name")
+        }
+)
+public class LaptopProducer{
+    @JsonProperty("id")
+    @Id
+    @SequenceGenerator(
+            name = "laptop_producer_sequence",
+            sequenceName = "laptop_producer_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "laptop_producer_sequence"
+    )
+    @Column(
+            name = "id",
+            updatable = false
+    )
+    private Long id;
+
+    @JsonProperty("name")
+    @Column(
+            name = "name",
+            columnDefinition = "TEXT"
+    )
+    private String name;
+
+    public LaptopProducer(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 }
