9a239550076aea3e4c05e4060b825853b251e4e7
Loredana27 <loredanastefania1227@gmail.com>
2022-07-13 16:27:28 +0300

    implement entity for Case

--- server/src/main/java/com/codecool/brightxchange/model/products/Case.java
+++ server/src/main/java/com/codecool/brightxchange/model/products/Case.java
@@ -1,5 +1,7 @@
 package com.codecool.brightxchange.model.products;
 
+import com.codecool.brightxchange.model.PriceCurrency;
+import com.codecool.brightxchange.model.Supplier;
 import com.codecool.brightxchange.model.producers.CaseProducer;
 import com.codecool.brightxchange.model.productTypes.CaseType;
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -10,22 +12,43 @@ import lombok.NoArgsConstructor;
 @Data
 @Builder
 @NoArgsConstructor
-public class Case extends BaseProduct {
+public class Case{
     @JsonProperty("caseId")
     private int id;
+
+    @JsonProperty("productPrice")
+    protected float price;
+
+    @JsonProperty("productCurrency")
+    protected PriceCurrency currency;
+
+    @JsonProperty("productQuantity")
+    protected int quantity;
+
+    @JsonProperty("productSupplier")
+    protected Supplier supplier;
+
     @JsonProperty("caseProducer")
     private CaseProducer caseProducer;
+
     @JsonProperty("caseModel")
     private String model;
+
     @JsonProperty("caseType")
     private CaseType caseType;
+
     @JsonProperty("caseNumberOfHDD")
     private int numberOfHDD;
+
     @JsonProperty("caseNumberOfSSD")
     private int numberOfSSD;
 
-    public Case(int id, CaseProducer caseProducer, String model, CaseType caseType, int numberOfHDD, int numberOfSSD) {
+    public Case(int id, float price, PriceCurrency currency, int quantity, Supplier supplier, CaseProducer caseProducer, String model, CaseType caseType, int numberOfHDD, int numberOfSSD) {
         this.id = id;
+        this.price = price;
+        this.currency = currency;
+        this.quantity = quantity;
+        this.supplier = supplier;
         this.caseProducer = caseProducer;
         this.model = model;
         this.caseType = caseType;
