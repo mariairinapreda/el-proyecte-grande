59aca2b32ce9d3d66eee7b671123920cd67b65bd
Loredana27 <loredanastefania1227@gmail.com>
2022-07-13 15:53:23 +0300

    implement entity for GCConnectionInterface

--- server/src/main/java/com/codecool/brightxchange/model/productRelated/GCConnectionInterface.java
+++ server/src/main/java/com/codecool/brightxchange/model/productRelated/GCConnectionInterface.java
@@ -5,16 +5,40 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
 @Data
 @Builder
 @NoArgsConstructor
+@Entity(name = "GCConnectionInterface")
+@Table(
+        name = "gc_connection_interface",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "name_unique", columnNames = "name")
+        }
+)
 public class GCConnectionInterface {
+
     @JsonProperty("gcConnectionInterfaceId")
-    private int id;
+    @Id
+    @SequenceGenerator(
+            name = "gc_connection_interface_sequence",
+            sequenceName = "gc_connection_interface_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "gc_connection_interface_sequence"
+    )
+    private Long id;
     @JsonProperty("gcConnectionInterfaceName")
+    @Column(
+            name = "name",
+            columnDefinition = "TEXT"
+    )
     private String name;
 
-    public GCConnectionInterface(int id, String name) {
+    public GCConnectionInterface(Long id, String name) {
         this.id = id;
         this.name = name;
     }
