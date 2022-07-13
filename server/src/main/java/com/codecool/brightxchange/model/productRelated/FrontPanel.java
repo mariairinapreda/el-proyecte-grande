10f6a607c4469111c18ea10c95436f7bd46d0285
Sidor Andrei <sidor.marian.andrei3001@gmail.com>
2022-07-13 15:55:48 +0300

    implement entity for FrontPanel

--- server/src/main/java/com/codecool/brightxchange/model/productRelated/FrontPanel.java
+++ server/src/main/java/com/codecool/brightxchange/model/productRelated/FrontPanel.java
@@ -5,26 +5,48 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
 @Data
 @Builder
 @NoArgsConstructor
+@Table(name = "front_panel")
+@Entity(name = "FrontPanel")
 public class FrontPanel {
+    @Id
+    @SequenceGenerator(
+            name = "front_panel_id_sequence",
+            sequenceName = "front_panel_id_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "front_panel_id_sequence"
+    )
+    @Column(
+            name = "id",
+            updatable = false
+    )
     @JsonProperty("frontPanelId")
-    private int id;
-    @JsonProperty("frontPanelNumberOfUSB2")
-    private int numberOfUSB2;
-    @JsonProperty("frontPanelNumberOfUSB3")
-    private int numberOfUSB3;
-    @JsonProperty("frontPanelNumberOfHeadphonesJack")
-    private int numberOfHeadphonesJack;
-    @JsonProperty("frontPanelNumberOfHMicrophoneJack")
-    private int numberOfHMicrophoneJack;
+    private Long id;
+    @Column(name = "number_of_usb2")
+    @JsonProperty("NumberOfUSB2")
+    private Integer numberOfUSB2;
+    @Column(name = "number_of_usb3")
+    @JsonProperty("NumberOfUSB3")
+    private Integer numberOfUSB3;
+    @Column(name = "number_of_headphones_jack")
+    @JsonProperty("NumberOfHeadphonesJack")
+    private Integer numberOfHeadphonesJack;
+    @Column(name = "number_of_microphone_jack")
+    @JsonProperty("NumberOfMicrophoneJack")
+    private Integer numberOfMicrophoneJack;
 
-    public FrontPanel(int id, int numberOfUSB2, int numberOfUSB3, int numberOfHeadphonesJack, int numberOfHMicrophoneJack) {
+    public FrontPanel(Long id, Integer numberOfUSB2, Integer numberOfUSB3, Integer numberOfHeadphonesJack, Integer numberOfMicrophoneJack) {
         this.id = id;
         this.numberOfUSB2 = numberOfUSB2;
         this.numberOfUSB3 = numberOfUSB3;
         this.numberOfHeadphonesJack = numberOfHeadphonesJack;
-        this.numberOfHMicrophoneJack = numberOfHMicrophoneJack;
+        this.numberOfMicrophoneJack = numberOfMicrophoneJack;
     }
 }
