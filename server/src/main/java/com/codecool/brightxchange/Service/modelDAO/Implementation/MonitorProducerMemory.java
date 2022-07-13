bd9ff786fb4dcdbe8f833fe65e4d0747c0020c07
Loredana27 <loredanastefania1227@gmail.com>
2022-07-13 09:39:29 +0300

    implement MonitorProducerMemory AND MonitorProducerService AND MonitorProducerDAO

---
+++ server/src/main/java/com/codecool/brightxchange/Service/modelDAO/Implementation/MonitorProducerMemory.java
@@ -0,0 +1,7 @@
+package com.codecool.brightxchange.Service.modelDAO.Implementation;
+import com.codecool.brightxchange.Service.modelDAO.DAO.MonitorProducerDAO;
+import org.springframework.stereotype.Repository;
+
+@Repository("MonitorProducerMemory")
+public class MonitorProducerMemory implements MonitorProducerDAO {
+}
