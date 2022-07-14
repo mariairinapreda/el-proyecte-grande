f7dfab569a34b34df9f655ca6ebe334372509441
SidorAndrei <sidor.marian.andrei3001@gmail.com>
2022-07-14 21:59:37 +0300

    add @CrossOrigin

--- server/src/main/java/com/codecool/brightxchange/controller/CategoryController.java
+++ server/src/main/java/com/codecool/brightxchange/controller/CategoryController.java
@@ -3,12 +3,16 @@ package com.codecool.brightxchange.controller;
 import com.codecool.brightxchange.model.categories.Category;
 import com.codecool.brightxchange.service.categories.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController("/categories")
+@CrossOrigin
+@RestController
+@RequestMapping("/categories")
 public class CategoryController {
     @Autowired
     private CategoryService service;
