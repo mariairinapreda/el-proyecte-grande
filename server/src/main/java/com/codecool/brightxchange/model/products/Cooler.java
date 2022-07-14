1991d41f72fdc905608453746abac68df8430ed8
Loredana27 <loredanastefania1227@gmail.com>
2022-07-14 09:14:02 +0300

    implement entity for Cooler

--- server/src/main/java/com/codecool/brightxchange/model/products/Cooler.java
+++ server/src/main/java/com/codecool/brightxchange/model/products/Cooler.java
@@ -6,18 +6,40 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
 @Data
 @Builder
 @NoArgsConstructor
+@Entity(name = "Cooler")
+@Table(name = "cooler")
 public class Cooler extends BaseProduct {
     @JsonProperty("coolerId")
-    private int id;
+    @Id
+    @SequenceGenerator(
+            name = "cooler_sequence",
+            sequenceName = "cooler_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "cooler_sequence"
+    )
+    private Long id;
     @JsonProperty("coolerDescription")
+    @Column(
+            name = "descripton",
+            columnDefinition = "TEXT"
+    )
     private String description;
     @JsonProperty("COOLERDimensions")
+    @Column(
+            name = "dimensions",
+            columnDefinition = "TEXT"
+    )
     private String dimensions;
 
-    public Cooler(int id, String description, String dimensions) {
+    public Cooler(Long id, String description, String dimensions) {
         this.id = id;
         this.description = description;
         this.dimensions = dimensions;
