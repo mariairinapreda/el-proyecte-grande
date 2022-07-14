b806c00e1928761bc05ba3830415eafa63d6ab35
Loredana27 <loredanastefania1227@gmail.com>
2022-07-14 19:56:00 +0300

    implement entity for GCSeries

--- server/src/main/java/com/codecool/brightxchange/model/productRelated/GCSeries.java
+++ server/src/main/java/com/codecool/brightxchange/model/productRelated/GCSeries.java
@@ -1,11 +1,14 @@
 package com.codecool.brightxchange.model.productRelated;
 
+import com.codecool.brightxchange.model.products.GraphicsCard;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Builder
 @Data
@@ -48,9 +51,23 @@ public class GCSeries {
     )
     private String name;
 
+    @OneToMany(
+            orphanRemoval = true,
+            cascade = {CascadeType.PERSIST, CascadeType.REMOVE},
+            fetch = FetchType.LAZY
+    )
+    private Set<GraphicsCard> graphicsCards = new HashSet<>();
+
     public GCSeries(int id, GraphicsCardFamily family, String name) {
         this.id = id;
         this.family = family;
         this.name = name;
+    }
+
+    public GCSeries(int id, GraphicsCardFamily family, String name, Set<GraphicsCard> graphicsCards) {
+        this.id = id;
+        this.family = family;
+        this.name = name;
+        this.graphicsCards = graphicsCards;
     }
 }
