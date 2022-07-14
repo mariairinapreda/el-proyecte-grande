9c186834338617b3f8592e3bc5b940ed4b6d429b
Loredana27 <loredanastefania1227@gmail.com>
2022-07-14 20:02:21 +0300

    implement entity for Vent

--- server/src/main/java/com/codecool/brightxchange/model/products/Vent.java
+++ server/src/main/java/com/codecool/brightxchange/model/products/Vent.java
@@ -1,32 +1,108 @@
 package com.codecool.brightxchange.model.products;
 
+import com.codecool.brightxchange.model.PriceCurrency;
+import com.codecool.brightxchange.model.Supplier;
 import com.codecool.brightxchange.model.producers.VentProducer;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
 @Data
 @Builder
 @NoArgsConstructor
-public class Vent extends BaseProduct {
+@Entity(name = "Vent")
+@Table(name = "vent")
+public class Vent {
     @JsonProperty("ventId")
-    private int id;
-    @JsonProperty("ventRotationSpeed")
-    private int rotationSpeed;
-    @JsonProperty("ventsNumber")
-    private int ventsNumber;
-    @JsonProperty("ventDescription")
-    private String description;
-    @JsonProperty("ventDimensions")
-    private String dimensions;
-    @JsonProperty("ventProducer")
-    private VentProducer ventProducer;
-    @JsonProperty("ventDimensions")
-    private int ventDimensions;
+    @Id
+    @SequenceGenerator(
+            name = "vent_sequence",
+            sequenceName = "vent_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "vent_sequence"
+    )
+    private Long id;
 
-    public Vent(int id, int rotationSpeed, int ventsNumber, String description, String dimensions, VentProducer ventProducer, int ventDimensions) {
+    @JsonProperty("productPrice")
+    private float price;
+
+    @JsonProperty("productCurrency")
+    @ManyToOne
+    @JoinColumn(
+            name = "price_currency_id",
+            nullable = false,
+            referencedColumnName = "id",
+            foreignKey = @ForeignKey(
+                    name = "vent_price_currency_fk"
+            )
+    )
+    private PriceCurrency currency;
+
+    @JsonProperty("productQuantity")
+    private Integer quantity;
+
+    @JsonProperty("productSupplier")
+    @ManyToOne
+    @JoinColumn(
+            name = "supplier_id",
+            nullable = false,
+            referencedColumnName = "id",
+            foreignKey = @ForeignKey(
+                    name = "vent_supplier_fk"
+            )
+    )
+    private Supplier supplier;
+
+    @JsonProperty("ventRotationSpeed")
+    @Column(name = "rotation_speed")
+    private Integer rotationSpeed;
+
+    @JsonProperty("ventsNumber")
+    @Column(name = "vents_number")
+    private Integer ventsNumber;
+
+    @JsonProperty("ventDescription")
+    @Column(
+            name = "description",
+            columnDefinition = "TEXT"
+    )
+    private String description;
+
+    @JsonProperty("ventDimensions")
+    @Column(
+            name = "dimensions",
+            columnDefinition = "TEXT"
+    )
+    private String dimensions;
+
+    @JsonProperty("ventProducer")
+    @ManyToOne
+    @JoinColumn(
+            name = "vent_producer_id",
+            nullable = false,
+            referencedColumnName = "id",
+            foreignKey = @ForeignKey(
+                    name = "vent_vent_producer_fk"
+            )
+    )
+    private VentProducer ventProducer;
+
+    @JsonProperty("ventDimensions")
+    @Column(name = "vent_dimensions")
+    private Integer ventDimensions;
+
+    public Vent(Long id, float price, PriceCurrency currency, Integer quantity, Supplier supplier, Integer rotationSpeed, Integer ventsNumber, String description, String dimensions, VentProducer ventProducer, Integer ventDimensions) {
         this.id = id;
+        this.price = price;
+        this.currency = currency;
+        this.quantity = quantity;
+        this.supplier = supplier;
         this.rotationSpeed = rotationSpeed;
         this.ventsNumber = ventsNumber;
         this.description = description;
