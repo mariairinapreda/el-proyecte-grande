ea194ffa9f1cda3cd4b6f1806b2787c3e1164307
Loredana27 <loredanastefania1227@gmail.com>
2022-07-14 19:57:20 +0300

    implement entity for GraphicsCardProducer

--- server/src/main/java/com/codecool/brightxchange/model/producers/GraphicsCardProducer.java
+++ server/src/main/java/com/codecool/brightxchange/model/producers/GraphicsCardProducer.java
@@ -1,11 +1,14 @@
 package com.codecool.brightxchange.model.producers;
 
+import com.codecool.brightxchange.model.products.GraphicsCard;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 @Builder
@@ -42,8 +45,21 @@ public class GraphicsCardProducer{
     )
     private String name;
 
+    @OneToMany(
+            orphanRemoval = true,
+            cascade = {CascadeType.PERSIST, CascadeType.REMOVE},
+            fetch = FetchType.LAZY
+    )
+    private Set<GraphicsCard> graphicsCards = new HashSet<>();
+
     public GraphicsCardProducer(Long id, String name) {
         this.id = id;
         this.name = name;
+    }
+
+    public GraphicsCardProducer(Long id, String name, Set<GraphicsCard> graphicsCards) {
+        this.id = id;
+        this.name = name;
+        this.graphicsCards = graphicsCards;
     }
 }
