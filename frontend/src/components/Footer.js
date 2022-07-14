f2ee64b38665bb315d5c81c5f4ae587d86d98abb
SidorAndrei <sidor.marian.andrei3001@gmail.com>
2022-07-14 23:20:54 +0300

    update footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -4,17 +4,16 @@ function Footer() {
 
 	return (
 		<div className="footer-div">
-			<div><span>L.A Team &copy; 2022 Codecool Bucharest</span></div>
+			<div><span>L.M.A Team &copy; 2022 Codecool Bucharest</span></div>
 			<div>
-				<a href="https://www.linkedin.com/in/loredana-mihai-b5b46b206/" target="_blank" rel="noreferrer">Loredana-Stefania
-					Mihai </a>
+				<a href="https://www.linkedin.com/in/loredana-mihai-b5b46b206/" target="_blank" rel="noreferrer">
+					Loredana-Stefania Mihai </a>
 				|
-				<a href="https://www.linkedin.com/in/loredana-mihai-b5b46b206/" target="_blank"
-				   rel="noreferrer"> Maria-Irina Preda </a>
+				<a href="https://www.linkedin.com/in/loredana-mihai-b5b46b206/" target="_blank" rel="noreferrer">
+					Maria-Irina Preda </a>
 				|
-				<a href="https://www.linkedin.com/in/marian-andrei-sidor-603069174/" target="_blank"
-				   rel="noreferrer"> Marian-Andrei
-					Sidor</a>
+				<a href="https://www.linkedin.com/in/marian-andrei-sidor-603069174/" target="_blank" rel="noreferrer">
+					Marian-Andrei Sidor</a>
 			</div>
 		</div>
 	)
