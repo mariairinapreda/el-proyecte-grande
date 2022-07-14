d3a8fb017933cf03eb22a3740d2fb194cfa76d0a
Loredana27 <loredanastefania1227@gmail.com>
2022-07-14 20:03:39 +0300

    implement entity for VideoBackPanel

--- server/src/main/java/com/codecool/brightxchange/model/productRelated/VideoBackPanel.java
+++ server/src/main/java/com/codecool/brightxchange/model/productRelated/VideoBackPanel.java
@@ -1,11 +1,15 @@
 package com.codecool.brightxchange.model.productRelated;
 
+import com.codecool.brightxchange.model.products.GraphicsCard;
+import com.codecool.brightxchange.model.products.Monitor;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Builder
 @Data
@@ -41,6 +45,20 @@ public class VideoBackPanel {
     @JsonProperty("videoBackPanelNumberOfJack")
     private Integer numberOfJack;
 
+    @OneToMany(
+            orphanRemoval = true,
+            cascade = {CascadeType.PERSIST, CascadeType.REMOVE},
+            fetch = FetchType.LAZY
+    )
+    private Set<GraphicsCard> graphicsCards = new HashSet<>();
+
+    @OneToMany(
+            orphanRemoval = true,
+            cascade = {CascadeType.PERSIST, CascadeType.REMOVE},
+            fetch = FetchType.LAZY
+    )
+    private Set<Monitor> monitors = new HashSet<>();
+
     public VideoBackPanel(Long id, Integer numberOfVGA, Integer numberOfDVI, Integer numberOfHDMI, Integer numberOfDisplayPort, Integer numberOfJack) {
         this.id = id;
         this.numberOfVGA = numberOfVGA;
@@ -48,5 +66,16 @@ public class VideoBackPanel {
         this.numberOfHDMI = numberOfHDMI;
         this.numberOfDisplayPort = numberOfDisplayPort;
         this.numberOfJack = numberOfJack;
+    }
+
+    public VideoBackPanel(Long id, Integer numberOfVGA, Integer numberOfDVI, Integer numberOfHDMI, Integer numberOfDisplayPort, Integer numberOfJack, Set<GraphicsCard> graphicsCards, Set<Monitor> monitors) {
+        this.id = id;
+        this.numberOfVGA = numberOfVGA;
+        this.numberOfDVI = numberOfDVI;
+        this.numberOfHDMI = numberOfHDMI;
+        this.numberOfDisplayPort = numberOfDisplayPort;
+        this.numberOfJack = numberOfJack;
+        this.graphicsCards = graphicsCards;
+        this.monitors = monitors;
     }
 }
