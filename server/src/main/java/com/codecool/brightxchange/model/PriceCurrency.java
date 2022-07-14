d8865082564cc7cc24280fdf9dbf4cc6e106dda6
mariairinapreda <mariairina.predaeu@gmail.com>
2022-07-14 12:45:13 +0300

    Entity annotations for PriceCurrency and Supplier

--- server/src/main/java/com/codecool/brightxchange/model/PriceCurrency.java
+++ server/src/main/java/com/codecool/brightxchange/model/PriceCurrency.java
@@ -4,15 +4,43 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 import java.util.Currency;
 
 
 @Data
 @NoArgsConstructor
 @Builder
+@Entity(name="PriceCurrency")
+@Table(name="price_currency",
+uniqueConstraints = {
+        @UniqueConstraint(name="price_currency_unique", columnNames = "currency")
+}
+)
 public class PriceCurrency {
+    @Id
+    @SequenceGenerator(
+            name = "price_currency_id_sequence",
+            sequenceName = "price_currency_id_sequence",
+            allocationSize = 1
+
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "price_currency_id_sequence"
+    )
+    @Column(
+            name = "id",
+            updatable = false
+    )
     @JsonProperty("priceId")
     private int id;
+
+    @Column(
+            name = "currency",
+            columnDefinition = "TEXT"
+    )
     @JsonProperty("priceCurrency")
     private Currency currency;
 
--- server/src/main/java/com/codecool/brightxchange/model/Supplier.java
+++ server/src/main/java/com/codecool/brightxchange/model/Supplier.java
@@ -5,12 +5,38 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
 @Data
 @NoArgsConstructor
 @Builder
+@Entity(name = "Supplier")
+@Table(name = "supplier",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "supplier_name_unique", columnNames = "name")
+        }
+)
 public class Supplier {
+    @Id
+    @SequenceGenerator(
+            name = "supplier_id_sequence",
+            sequenceName = "supplier_id_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "supplier_id_sequence"
+    )
+    @Column(
+            name = "id",
+            updatable = false
+    )
     @JsonProperty("supplierId")
     private int id;
+    @Column(
+            name = "name",
+            columnDefinition = "TEXT"
+    )
     @JsonProperty("supplierName")
     private String name;
 
