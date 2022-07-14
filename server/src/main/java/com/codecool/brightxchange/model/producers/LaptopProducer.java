f53e4caa6a8cf94efb2c4451e790f3a4cff42620
Loredana27 <loredanastefania1227@gmail.com>
2022-07-14 19:57:57 +0300

    implement entity for LaptopProducer

--- server/src/main/java/com/codecool/brightxchange/model/producers/LaptopProducer.java
+++ server/src/main/java/com/codecool/brightxchange/model/producers/LaptopProducer.java
@@ -1,11 +1,14 @@
 package com.codecool.brightxchange.model.producers;
 
+import com.codecool.brightxchange.model.products.Laptop;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 @Builder
@@ -42,8 +45,21 @@ public class LaptopProducer{
     )
     private String name;
 
+    @OneToMany(
+            orphanRemoval = true,
+            cascade = {CascadeType.PERSIST, CascadeType.REMOVE},
+            fetch = FetchType.LAZY
+    )
+    private Set<Laptop> laptops = new HashSet<>();
+
     public LaptopProducer(Long id, String name) {
         this.id = id;
         this.name = name;
+    }
+
+    public LaptopProducer(Long id, String name, Set<Laptop> laptops) {
+        this.id = id;
+        this.name = name;
+        this.laptops = laptops;
     }
 }
