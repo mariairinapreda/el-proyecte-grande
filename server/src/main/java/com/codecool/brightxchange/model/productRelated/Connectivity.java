dd6719e2f2274c9e908ce2961fe9044f2a1d6470
Loredana27 <loredanastefania1227@gmail.com>
2022-07-14 19:54:32 +0300

    implement entity for Connectivity

--- server/src/main/java/com/codecool/brightxchange/model/productRelated/Connectivity.java
+++ server/src/main/java/com/codecool/brightxchange/model/productRelated/Connectivity.java
@@ -1,11 +1,14 @@
 package com.codecool.brightxchange.model.productRelated;
 
+import com.codecool.brightxchange.model.products.Laptop;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 @Builder
@@ -41,8 +44,21 @@ public class Connectivity {
     @JsonProperty("type")
     private String type;
 
+    @OneToMany(
+            orphanRemoval = true,
+            cascade = {CascadeType.PERSIST, CascadeType.REMOVE},
+            fetch = FetchType.LAZY
+    )
+    private Set<Laptop> laptops = new HashSet<>();
+
     public Connectivity(Long id, String type) {
         this.id = id;
         this.type = type;
+    }
+
+    public Connectivity(Long id, String type, Set<Laptop> laptops) {
+        this.id = id;
+        this.type = type;
+        this.laptops = laptops;
     }
 }
