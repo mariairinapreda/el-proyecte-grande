986b02d3bc626f40e341ab746495a91549cef385
Loredana27 <loredanastefania1227@gmail.com>
2022-07-14 19:58:42 +0300

    implement entity for Motherboard

--- server/src/main/java/com/codecool/brightxchange/model/products/Motherboard.java
+++ server/src/main/java/com/codecool/brightxchange/model/products/Motherboard.java
@@ -1,5 +1,7 @@
 package com.codecool.brightxchange.model.products;
 
+import com.codecool.brightxchange.model.PriceCurrency;
+import com.codecool.brightxchange.model.Supplier;
 import com.codecool.brightxchange.model.producers.ChipsetProducer;
 import com.codecool.brightxchange.model.producers.MotherboardProducer;
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -7,12 +9,45 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.ForeignKey;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
 @Data
 @NoArgsConstructor
 @Builder
-public class Motherboard extends BaseProduct {
+public class Motherboard {
     @JsonProperty("motherboardId")
     private int id;
+    @JsonProperty("productPrice")
+    private float price;
+
+    @JsonProperty("productCurrency")
+    @ManyToOne
+    @JoinColumn(
+            name = "price_currency_id",
+            nullable = false,
+            referencedColumnName = "id",
+            foreignKey = @ForeignKey(
+                    name = "cooler_price_currency_fk"
+            )
+    )
+    private PriceCurrency currency;
+
+    @JsonProperty("productQuantity")
+    private Integer quantity;
+
+    @JsonProperty("productSupplier")
+    @ManyToOne
+    @JoinColumn(
+            name = "supplier_id",
+            nullable = false,
+            referencedColumnName = "id",
+            foreignKey = @ForeignKey(
+                    name = "cooler_supplier_fk"
+            )
+    )
+    private Supplier supplier;
     @JsonProperty("motherboardChipsetProducer")
     private ChipsetProducer chipsetProducer;
     @JsonProperty("motherboardProducer")
