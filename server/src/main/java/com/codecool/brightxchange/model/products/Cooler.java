b6264660c39c8713356a3aac0c8be1b5cc81f71d
Loredana27 <loredanastefania1227@gmail.com>
2022-07-14 19:54:20 +0300

    implement entity for Cooler

--- server/src/main/java/com/codecool/brightxchange/model/products/Cooler.java
+++ server/src/main/java/com/codecool/brightxchange/model/products/Cooler.java
@@ -1,6 +1,7 @@
 package com.codecool.brightxchange.model.products;
 
-import com.codecool.brightxchange.model.products.BaseProduct;
+import com.codecool.brightxchange.model.PriceCurrency;
+import com.codecool.brightxchange.model.Supplier;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
@@ -13,7 +14,7 @@ import javax.persistence.*;
 @NoArgsConstructor
 @Entity(name = "Cooler")
 @Table(name = "cooler")
-public class Cooler extends BaseProduct {
+public class Cooler {
     @JsonProperty("coolerId")
     @Id
     @SequenceGenerator(
@@ -26,6 +27,37 @@ public class Cooler extends BaseProduct {
             generator = "cooler_sequence"
     )
     private Long id;
+
+    @JsonProperty("productPrice")
+    private float price;
+
+    @JsonProperty("productCurrency")
+    @ManyToOne
+    @JoinColumn(
+            name = "price_currency_id",
+            nullable = false,
+            referencedColumnName = "id",
+            foreignKey = @ForeignKey(
+                    name = "cooler_price_currency_fk"
+            )
+    )
+    private PriceCurrency currency;
+
+    @JsonProperty("productQuantity")
+    private Integer quantity;
+
+    @JsonProperty("productSupplier")
+    @ManyToOne
+    @JoinColumn(
+            name = "supplier_id",
+            nullable = false,
+            referencedColumnName = "id",
+            foreignKey = @ForeignKey(
+                    name = "cooler_supplier_fk"
+            )
+    )
+    private Supplier supplier;
+
     @JsonProperty("coolerDescription")
     @Column(
             name = "descripton",
