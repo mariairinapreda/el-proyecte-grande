0bbb12a5fe81b2d510bd8ef87a161c7e7e34094b
Loredana27 <loredanastefania1227@gmail.com>
2022-07-14 19:59:32 +0300

    implement entity for PowerSupply

--- server/src/main/java/com/codecool/brightxchange/model/products/PowerSupply.java
+++ server/src/main/java/com/codecool/brightxchange/model/products/PowerSupply.java
@@ -1,5 +1,7 @@
 package com.codecool.brightxchange.model.products;
 
+import com.codecool.brightxchange.model.PriceCurrency;
+import com.codecool.brightxchange.model.Supplier;
 import com.codecool.brightxchange.model.producers.PowerSupplyProducer;
 import com.codecool.brightxchange.model.productTypes.PowerSupplyType;
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -7,23 +9,93 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
 @Data
 @Builder
 @NoArgsConstructor
-public class PowerSupply extends BaseProduct {
+@Entity(name = "PowerSupply")
+@Table(name = "power_supply")
+public class PowerSupply {
     @JsonProperty("powerSupplyId")
-    private int id;
+    @Id
+    @SequenceGenerator(
+            name = "power_supply_sequence",
+            sequenceName = "power_supply_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "power_supply_sequence"
+    )
+    private Long id;
+
+    @JsonProperty("productPrice")
+    private float price;
+
+    @JsonProperty("productCurrency")
+    @ManyToOne
+    @JoinColumn(
+            name = "price_currency_id",
+            nullable = false,
+            referencedColumnName = "id",
+            foreignKey = @ForeignKey(
+                    name = "power_supply_price_currency_fk"
+            )
+    )
+    private PriceCurrency currency;
+
+    @JsonProperty("productQuantity")
+    private Integer quantity;
+
+    @JsonProperty("productSupplier")
+    @ManyToOne
+    @JoinColumn(
+            name = "supplier_id",
+            nullable = false,
+            referencedColumnName = "id",
+            foreignKey = @ForeignKey(
+                    name = "power_supply_supplier_fk"
+            )
+    )
+    private Supplier supplier;
     @JsonProperty("powerSupplyProducer")
+    @ManyToOne
+    @JoinColumn(
+            name = "producer_id",
+            nullable = false,
+            referencedColumnName = "id",
+            foreignKey = @ForeignKey(
+                    name = "power_supply_power_supply_producer_fk"
+            )
+    )
     private PowerSupplyProducer powerSupplyProducer;
     @JsonProperty("powerSupplyType")
+    @ManyToOne
+    @JoinColumn(
+            name = "type_id",
+            nullable = false,
+            referencedColumnName = "id",
+            foreignKey = @ForeignKey(
+                    name = "power_supply_power_supply_type_fk"
+            )
+    )
     private PowerSupplyType powerSupplyType;
     @JsonProperty("powerSupplyPower")
-    private int power;
+    private Integer power;
     @JsonProperty("powerSupplyCertification")
+    @Column(
+            name = "certification",
+            columnDefinition = "TEXT"
+    )
     private String certification;
 
-    public PowerSupply(int id, PowerSupplyProducer powerSupplyProducer, PowerSupplyType powerSupplyType, int power, String certification) {
+    public PowerSupply(Long id, float price, PriceCurrency currency, Integer quantity, Supplier supplier, PowerSupplyProducer powerSupplyProducer, PowerSupplyType powerSupplyType, Integer power, String certification) {
         this.id = id;
+        this.price = price;
+        this.currency = currency;
+        this.quantity = quantity;
+        this.supplier = supplier;
         this.powerSupplyProducer = powerSupplyProducer;
         this.powerSupplyType = powerSupplyType;
         this.power = power;
