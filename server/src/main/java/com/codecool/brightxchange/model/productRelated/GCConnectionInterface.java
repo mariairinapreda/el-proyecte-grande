f7588eecccd1a3c1087e6c779921bf4bff47f29f
Loredana27 <loredanastefania1227@gmail.com>
2022-07-14 19:55:23 +0300

    implement entity for GCConnectionInterface

--- server/src/main/java/com/codecool/brightxchange/model/productRelated/GCConnectionInterface.java
+++ server/src/main/java/com/codecool/brightxchange/model/productRelated/GCConnectionInterface.java
@@ -1,11 +1,14 @@
 package com.codecool.brightxchange.model.productRelated;
 
+import com.codecool.brightxchange.model.products.GraphicsCard;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 @Builder
@@ -38,8 +41,22 @@ public class GCConnectionInterface {
     )
     private String name;
 
+
+    @OneToMany(
+            orphanRemoval = true,
+            cascade = {CascadeType.PERSIST, CascadeType.REMOVE},
+            fetch = FetchType.LAZY
+    )
+    private Set<GraphicsCard> graphicsCards = new HashSet<>();
+
     public GCConnectionInterface(Long id, String name) {
         this.id = id;
         this.name = name;
+    }
+
+    public GCConnectionInterface(Long id, String name, Set<GraphicsCard> graphicsCards) {
+        this.id = id;
+        this.name = name;
+        this.graphicsCards = graphicsCards;
     }
 }
