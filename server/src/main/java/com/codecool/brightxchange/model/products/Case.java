aa2e2b6b166611e16f5ae85647983a495af2dde7
Loredana27 <loredanastefania1227@gmail.com>
2022-07-14 09:13:26 +0300

    implement entity for Case

--- server/src/main/java/com/codecool/brightxchange/model/products/Case.java
+++ server/src/main/java/com/codecool/brightxchange/model/products/Case.java
@@ -9,12 +9,26 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
 @Data
 @Builder
 @NoArgsConstructor
+@Entity(name = "Case")
+@Table(name = "case")
 public class Case{
     @JsonProperty("caseId")
-    private int id;
+    @Id
+    @SequenceGenerator(
+            name = "case_sequence",
+            sequenceName = "case_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "case_sequence"
+    )
+    private Long id;
 
     @JsonProperty("productPrice")
     protected float price;
@@ -23,27 +37,60 @@ public class Case{
     protected PriceCurrency currency;
 
     @JsonProperty("productQuantity")
-    protected int quantity;
+    protected Integer quantity;
 
     @JsonProperty("productSupplier")
-    protected Supplier supplier;
+    @ManyToOne
+    @JoinColumn(
+            name = "supplier_id",
+            nullable = false,
+            referencedColumnName = "id",
+            foreignKey = @ForeignKey(
+                    name = "case_supplier_fk"
+            )
+    )
+    private Supplier supplier;
 
     @JsonProperty("caseProducer")
+    @ManyToOne
+    @JoinColumn(
+            name = "case_producer_id",
+            nullable = false,
+            referencedColumnName = "id",
+            foreignKey = @ForeignKey(
+                    name = "case_case_producer_fk"
+            )
+    )
     private CaseProducer caseProducer;
 
     @JsonProperty("caseModel")
+    @Column(   
+            name =  "model",
+            columnDefinition = "TEXT"
+    )
     private String model;
 
     @JsonProperty("caseType")
+    @ManyToOne
+    @JoinColumn(
+            name = "case_type_id",
+            nullable = false,
+            referencedColumnName = "id",
+            foreignKey = @ForeignKey(
+                    name = "case_case_type_fk"
+            )
+    )
     private CaseType caseType;
 
     @JsonProperty("caseNumberOfHDD")
-    private int numberOfHDD;
+    @Column(name = "number_of_hdd")
+    private Integer numberOfHDD;
 
     @JsonProperty("caseNumberOfSSD")
-    private int numberOfSSD;
+    @Column(name = "number_of_ssd")
+    private Integer numberOfSSD;
 
-    public Case(int id, float price, PriceCurrency currency, int quantity, Supplier supplier, CaseProducer caseProducer, String model, CaseType caseType, int numberOfHDD, int numberOfSSD) {
+    public Case(Long id, float price, PriceCurrency currency, Integer quantity, Supplier supplier, CaseProducer caseProducer, String model, CaseType caseType, Integer numberOfHDD, Integer numberOfSSD) {
         this.id = id;
         this.price = price;
         this.currency = currency;
