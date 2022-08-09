0f3d56aab416990ab2d51099ef539f5b68bd700f
Sidor Andrei <sidor.marian.andrei3001@gmail.com>
2022-08-09 13:45:52 +0300

    refactor

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,36 @@
-// import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LandingPage from "./componentPages/LandingPage";
+import CasePage from "./componentPages/CasePage";
+import Footer from "./components/Footer";
+import React from "react";
 
 function App() {
   return (
-    <div></div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+
+        <Route path="/carcase" element={<CasePage />} />
+        <Route path="/coolere" element={<LandingPage />} />
+        <Route path="/desktop-uri" element={<LandingPage />} />
+        <Route path="/hard-disk-uri" element={<LandingPage />} />
+        <Route path="/laptopuri" element={<LandingPage />} />
+        <Route path="/memorii" element={<LandingPage />} />
+        <Route path="/placi-de-baza" element={<LandingPage />} />
+        <Route path="/placi-video" element={<LandingPage />} />
+        <Route path="/procesoare" element={<LandingPage />} />
+        <Route path="/ssd-uri" element={<LandingPage />} />
+        <Route path="/surse" element={<LandingPage />} />
+        <Route path="/ventilatoare" element={<LandingPage />} />
+
+        <Route path="/add-case" element={<Footer />} />
+        <Route path="/cases" element={<Footer />}>
+          <Route path=":caseId" element={<Footer />}></Route>
+        </Route>
+        <Route path="*" element={<Footer />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,50 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
-import {
-    BrowserRouter,
-    Routes,
-    Route,
-} from "react-router-dom";
-import LandingPage from "./componentPages/LandingPage";
-import Footer from "./components/Footer";
-import CasePage from "./componentPages/CasePage";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-      <BrowserRouter>
-          <Routes>
-              <Route path="/" element={<LandingPage />} />
-
-              <Route path="/carcase" element={<CasePage />} />
-              <Route path="/coolere" element={<LandingPage />} />
-              <Route path="/desktop-uri" element={<LandingPage />} />
-              <Route path="/hard-disk-uri" element={<LandingPage />} />
-              <Route path="/laptopuri" element={<LandingPage />} />
-              <Route path="/memorii" element={<LandingPage />} />
-              <Route path="/placi-de-baza" element={<LandingPage />} />
-              <Route path="/placi-video" element={<LandingPage />} />
-              <Route path="/procesoare" element={<LandingPage />} />
-              <Route path="/ssd-uri" element={<LandingPage />} />
-              <Route path="/surse" element={<LandingPage />} />
-              <Route path="/ventilatoare" element={<LandingPage />} />
-
-              <Route path="/add-case" element={<Footer />}/>
-              <Route path="/cases" element={<Footer />}>
-                  <Route path=":caseId" element={<Footer />}></Route>
-              </Route>
-              <Route path="*" element={<Footer />}></Route>
-          </Routes>
-      </BrowserRouter>
-
+    <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
