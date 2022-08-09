1e1989ebbb6b922ea49f4e4a89da7ce358fb1e5b
Sidor Andrei <sidor.marian.andrei3001@gmail.com>
2022-08-09 15:39:34 +0300

    added case page design

--- frontend/src/componentPages/CasePage.js
+++ frontend/src/componentPages/CasePage.js
@@ -1,139 +1,114 @@
-import "../scss/products_page.scss";
+import Card from "../wrappers/card/Card";
+import CaseDetails from "../wrappers/card/details/case-details/CaseDetails";
+import ProductsContainer from "../wrappers/products-container/ProductsContainer";
 
 const CasePage = () => {
-  // const [cases,setCases] = useState([]);
+	// const [cases,setCases] = useState([]);
 
-  // useEffect(() => {
-  //     fetch("http://localhost:8888/case", {method:"GET"})
-  //         .then(response => response.json())
-  //         .then(re => setCases(re))
-  // },[])
-  let cases = [
-    {
-      price: 599.9,
-      currency: {
-        currency: "RON",
-      },
-      quantity: 3,
-      supplier: {
-        name: "Bright Xchange",
-      },
-      caseProducer: {
-        name: "Lenovo",
-      },
-      model: "X450",
-      caseType: {
-        name: "High Tower",
-      },
-      numberOfHDD: 3,
-      numberOfSSD: 2,
-      imageUrl:
-        "https://brightxchangeprojectbucket.s3.eu-central-1.amazonaws.com/product_26.jpg",
-    },
-    {
-      price: 233.9,
-      currency: {
-        currency: "RON",
-      },
-      quantity: 5,
-      supplier: {
-        name: "Bright Xchange",
-      },
-      caseProducer: {
-        name: "Lenovo",
-      },
-      model: "X300",
-      caseType: {
-        name: "High Tower",
-      },
-      numberOfHDD: 3,
-      numberOfSSD: 2,
-      imageUrl:
-        "https://brightxchangeprojectbucket.s3.eu-central-1.amazonaws.com/product_26.jpg",
-    },
-    {
-      price: 135.9,
-      currency: {
-        currency: "RON",
-      },
-      quantity: 2,
-      supplier: {
-        name: "Bright Xchange",
-      },
-      caseProducer: {
-        name: "Myria",
-      },
-      model: "X300",
-      caseType: {
-        name: "High Tower",
-      },
-      numberOfHDD: 3,
-      numberOfSSD: 2,
-      imageUrl:
-        "https://brightxchangeprojectbucket.s3.eu-central-1.amazonaws.com/product_26.jpg",
-    },
-    {
-      price: 123.3,
-      currency: {
-        currency: "RON",
-      },
-      quantity: 2,
-      supplier: {
-        name: "Bright Xchange",
-      },
-      caseProducer: {
-        name: "Alienware",
-      },
-      model: "X300",
-      caseType: {
-        name: "Middle Tower",
-      },
-      numberOfHDD: 3,
-      numberOfSSD: 2,
-      imageUrl:
-        "https://brightxchangeprojectbucket.s3.eu-central-1.amazonaws.com/product_26.jpg",
-    },
-  ];
-  return (
-    <>
-      <div className="products">
-        <div className="cards-container">
-          {cases.map((e, i) => (
-            <div key={"case_" + i} className="custom-card">
-              <img className="custom-card-image" src={e.imageUrl} alt="None" />
-              <div className="custom-card-header">
-                <p className="custom-card-title">
-                  Carcasa {e.caseProducer.name} {e.caseType.name} {e.model}
-                </p>
-              </div>
-              <table className="custom-card-details">
-                <tbody>
-                  <tr>
-                    <td>Numar suport HDD: {e.numberOfHDD}</td>
-                  </tr>
-                  <tr>
-                    <td>Numar suport SSD: {e.numberOfSSD}</td>
-                  </tr>
-                </tbody>
-              </table>
-              <div className="custom-card-body">
-                <div className="custom-card-text">
-                  <p className="lead">
-                    {e.price} {e.currency.currency}
-                  </p>
-                </div>
-                <div className="custom-card-text">
-                  <button className="btn btn-success add-product">
-                    Adauga in cos
-                  </button>
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
-    </>
-  );
+	// useEffect(() => {
+	//     fetch("http://localhost:8888/case", {method:"GET"})
+	//         .then(response => response.json())
+	//         .then(re => setCases(re))
+	// },[])
+	let cases = [
+		{
+			price: 599.9,
+			currency: {
+				currency: "RON",
+			},
+			quantity: 3,
+			supplier: {
+				name: "Bright Xchange",
+			},
+			caseProducer: {
+				name: "Lenovo",
+			},
+			model: "X450",
+			caseType: {
+				name: "High Tower",
+			},
+			numberOfHDD: 3,
+			numberOfSSD: 2,
+			imageUrl:
+				"https://brightxchangeprojectbucket.s3.eu-central-1.amazonaws.com/product_26.jpg",
+		},
+		{
+			price: 233.9,
+			currency: {
+				currency: "RON",
+			},
+			quantity: 5,
+			supplier: {
+				name: "Bright Xchange",
+			},
+			caseProducer: {
+				name: "Lenovo",
+			},
+			model: "X300",
+			caseType: {
+				name: "High Tower",
+			},
+			numberOfHDD: 3,
+			numberOfSSD: 2,
+			imageUrl:
+				"https://brightxchangeprojectbucket.s3.eu-central-1.amazonaws.com/product_26.jpg",
+		},
+		{
+			price: 135.9,
+			currency: {
+				currency: "RON",
+			},
+			quantity: 2,
+			supplier: {
+				name: "Bright Xchange",
+			},
+			caseProducer: {
+				name: "Myria",
+			},
+			model: "X300",
+			caseType: {
+				name: "High Tower",
+			},
+			numberOfHDD: 3,
+			numberOfSSD: 2,
+			imageUrl:
+				"https://brightxchangeprojectbucket.s3.eu-central-1.amazonaws.com/product_26.jpg",
+		},
+		{
+			price: 123.3,
+			currency: {
+				currency: "RON",
+			},
+			quantity: 2,
+			supplier: {
+				name: "Bright Xchange",
+			},
+			caseProducer: {
+				name: "Alienware",
+			},
+			model: "X300",
+			caseType: {
+				name: "Middle Tower",
+			},
+			numberOfHDD: 3,
+			numberOfSSD: 2,
+			imageUrl:
+				"https://brightxchangeprojectbucket.s3.eu-central-1.amazonaws.com/product_26.jpg",
+		},
+	];
+	return (
+		<>
+			<ProductsContainer>
+					{cases.map((e, i) =>
+						<Card title={`Carcasa ${e.caseProducer} ${e.caseType} ${e.model}`}
+							  price={e.price}
+							  currency={e.currency}
+							  imageUrl={e.imageUrl}
+							  details={<CaseDetails numberOfHDD={e.numberOfHDD} numberOfSSD={e.numberOfSSD}/>}/>
+					)}
+			</ProductsContainer>
+		</>
+	);
 };
 
 export default CasePage;
---
+++ frontend/src/wrappers/card/Card.js
@@ -0,0 +1,29 @@
+
+
+const Card = ({title, imageUrl, details,price, currency}) => {
+  return (
+	  <div  className="custom-card">
+		  <img className="custom-card-image" src={imageUrl} alt="None" />
+		  <div className="custom-card-header">
+			  <p className="custom-card-title">
+				  {title}
+			  </p>
+		  </div>
+		  {details}
+		  <div className="custom-card-body">
+			  <div className="custom-card-text">
+				  <p className="lead">
+					  {price} {currency}
+				  </p>
+			  </div>
+			  <div className="custom-card-text">
+				  <button className="btn btn-success add-product">
+					  Adauga in cos
+				  </button>
+			  </div>
+		  </div>
+	  </div>
+  );
+}
+
+export default Card;
