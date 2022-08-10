b5ce3246fc4c463055a3241ab5325bbaaa3f577c
Loredana27 <loredanastefania1227@gmail.com>
2022-08-10 17:02:48 +0300

    update LaptopProducerDAO and LaptopProducerService

--- server/src/main/java/com/codecool/brightxchange/service/producers/LaptopProducerService.java
+++ server/src/main/java/com/codecool/brightxchange/service/producers/LaptopProducerService.java
@@ -1,8 +1,12 @@
 package com.codecool.brightxchange.service.producers;
 
+import com.codecool.brightxchange.model.producers.LaptopProducer;
 import com.codecool.brightxchange.repositories.producers.LaptopProducerDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 
 @Service("LaptopProducerService")
@@ -11,5 +15,21 @@ public class LaptopProducerService{
 
     public LaptopProducerService(LaptopProducerDAO laptopProducerDAO) {
         this.laptopProducerDAO = laptopProducerDAO;
+    }
+
+    public List<LaptopProducer> getAll(){
+        return laptopProducerDAO.findAll();
+    }
+
+    public Optional<LaptopProducer> getById(long id){
+        return laptopProducerDAO.findById(id);
+    }
+
+    public void addLaptopProducer(LaptopProducer tempLaptopProducer){
+        laptopProducerDAO.save(tempLaptopProducer);
+    }
+
+    public void updateLaptopProducer(long id, String name){
+        laptopProducerDAO.update(id, name);
     }
 }
