ab063386c88ffca6681a9b95d223cd6be79d84e8
mariairinapreda <mariairina.predaeu@gmail.com>
2022-08-10 16:32:12 +0300

    implemented CaseProducerDao

--- server/src/main/java/com/codecool/brightxchange/repositories/producers/CaseProducerDAO.java
+++ server/src/main/java/com/codecool/brightxchange/repositories/producers/CaseProducerDAO.java
@@ -1,9 +1,18 @@
 package com.codecool.brightxchange.repositories.producers;
 
+import com.codecool.brightxchange.model.PriceCurrency;
 import com.codecool.brightxchange.model.producers.CaseProducer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CaseProducerDAO  extends JpaRepository<CaseProducer, Long> {
+
+    @Modifying
+    @Query("update CaseProducer cp set  cp.name= :name  where cp.id= :id ")
+    void update(@Param("id") long id, @Param("name")String name);
+
 }
