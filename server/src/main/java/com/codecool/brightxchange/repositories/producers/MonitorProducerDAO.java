94241c94f869b6a912cab27404cbd30416737364
Loredana27 <loredanastefania1227@gmail.com>
2022-08-10 17:04:02 +0300

    update MonitorProducerDAO and MonitorProducerService

--- server/src/main/java/com/codecool/brightxchange/repositories/producers/MonitorProducerDAO.java
+++ server/src/main/java/com/codecool/brightxchange/repositories/producers/MonitorProducerDAO.java
@@ -2,8 +2,15 @@ package com.codecool.brightxchange.repositories.producers;
 
 import com.codecool.brightxchange.model.producers.MonitorProducer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MonitorProducerDAO extends JpaRepository<MonitorProducer, Long> {
+    @Modifying
+    @Query("update MonitorProducer mp set mp.name = :name where mp.id = :id")
+    void update(@Param("id") float id,
+                @Param("name") String name);
 }
--- server/src/main/java/com/codecool/brightxchange/service/producers/MonitorProducerService.java
+++ server/src/main/java/com/codecool/brightxchange/service/producers/MonitorProducerService.java
@@ -1,7 +1,11 @@
 package com.codecool.brightxchange.service.producers;
+import com.codecool.brightxchange.model.producers.MonitorProducer;
 import com.codecool.brightxchange.repositories.producers.MonitorProducerDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service("MonitorProducerService")
 public class MonitorProducerService {
@@ -9,5 +13,21 @@ public class MonitorProducerService {
 
     public MonitorProducerService(MonitorProducerDAO monitorProducerDAO) {
         this.monitorProducerDAO = monitorProducerDAO;
+    }
+
+    public List<MonitorProducer> getAll(){
+        return monitorProducerDAO.findAll();
+    }
+
+    public Optional<MonitorProducer> getById(long id){
+        return monitorProducerDAO.findById(id);
+    }
+
+    public void addMonitorProducer(MonitorProducer tempMonitorProducer){
+        monitorProducerDAO.save(tempMonitorProducer);
+    }
+
+    public void updateMonitorProducer(long id, String name){
+        monitorProducerDAO.update(id, name);
     }
 }
