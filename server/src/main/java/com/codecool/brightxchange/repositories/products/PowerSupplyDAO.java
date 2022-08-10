df705e2a4e41a0b7f1aafa871d4010c01deff853
Loredana27 <loredanastefania1227@gmail.com>
2022-08-10 17:05:41 +0300

    update PowerSupplyDAO and PowerSupplyService

--- server/src/main/java/com/codecool/brightxchange/repositories/products/PowerSupplyDAO.java
+++ server/src/main/java/com/codecool/brightxchange/repositories/products/PowerSupplyDAO.java
@@ -1,9 +1,45 @@
 package com.codecool.brightxchange.repositories.products;
 
+import com.codecool.brightxchange.model.Supplier;
+import com.codecool.brightxchange.model.producers.PowerSupplyProducer;
+import com.codecool.brightxchange.model.productTypes.PowerSupplyType;
 import com.codecool.brightxchange.model.products.PowerSupply;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PowerSupplyDAO extends JpaRepository<PowerSupply, Long> {
+    @Modifying
+    @Query("update PowerSupply ps set" +
+            " ps.price= :price," +
+            " ps.quantity = :quantity," +
+            " ps.certification = :certification," +
+            " ps.power = :power" +
+            " where ps.id= :id ")
+    void update(@Param("id") long id,
+                @Param("price") float price,
+                @Param("quantity") Integer quantity,
+                @Param("certification") String certification,
+                @Param("power") Integer power
+    );
+
+    @Modifying
+    @Query("update PowerSupply ps set ps.supplier =: supplier where ps.id = :id")
+    void updateSupplier(@Param("id") long id,
+                        @Param("supplier") Supplier supplier);
+
+
+    @Modifying
+    @Query("update PowerSupply ps set ps.powerSupplyProducer = :powerSupplyProducer where ps.id = :id")
+    void updatePowerSupplyProducer(@Param("id") long id,
+                                   @Param("powerSupplyProducer")PowerSupplyProducer powerSupplyProducer);
+
+
+    @Modifying
+    @Query("update PowerSupply ps set ps.powerSupplyType = :powerSupplyType where ps.id = :id")
+    void updatePowerSupplyType(@Param("id") long id,
+                               @Param("powerSupplyType")PowerSupplyType powerSupplyType);
 }
--- server/src/main/java/com/codecool/brightxchange/service/products/PowerSupplyService.java
+++ server/src/main/java/com/codecool/brightxchange/service/products/PowerSupplyService.java
@@ -1,8 +1,15 @@
 package com.codecool.brightxchange.service.products;
 
+import com.codecool.brightxchange.model.Supplier;
+import com.codecool.brightxchange.model.producers.PowerSupplyProducer;
+import com.codecool.brightxchange.model.productTypes.PowerSupplyType;
+import com.codecool.brightxchange.model.products.PowerSupply;
 import com.codecool.brightxchange.repositories.products.PowerSupplyDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service("PowerSupplyService")
 public class PowerSupplyService {
@@ -11,4 +18,45 @@ public class PowerSupplyService {
     public PowerSupplyService(PowerSupplyDAO powerSupplyDAO) {
         this.powerSupplyDAO = powerSupplyDAO;
     }
+
+    public List<PowerSupply> getAll(){
+       return powerSupplyDAO.findAll();
+    }
+
+    public Optional<PowerSupply> getById(long id){
+        return powerSupplyDAO.findById(id);
+    }
+
+    public void addPowerSupply(PowerSupply tempPowerSupply){
+        powerSupplyDAO.save(tempPowerSupply);
+    }
+
+    public void updatePowerSupply(long id,
+                                  float price,
+                                  Integer quantity,
+                                  String certification,
+                                  Integer power){
+        powerSupplyDAO.update(id,
+                price,
+                quantity,
+                certification,
+                power);
+    }
+
+    public void updateSupplier(long id, Supplier supplier){
+        powerSupplyDAO.updateSupplier(id, supplier);
+    }
+
+    public void updatePowerSupplyProducer(long id, PowerSupplyProducer powerSupplyProducer){
+        powerSupplyDAO.updatePowerSupplyProducer(id, powerSupplyProducer);
+    }
+
+    public void updatePowerSupplyType(long id, PowerSupplyType powerSupplyType){
+        powerSupplyDAO.updatePowerSupplyType(id, powerSupplyType);
+    }
+
+
+
+
+
 }
