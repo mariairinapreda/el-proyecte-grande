fe8244886a7d2c389e478a3737b6151529a60f91
Loredana27 <loredanastefania1227@gmail.com>
2022-08-10 17:04:50 +0300

    update MotherboardDAO and MotherboardService

--- server/src/main/java/com/codecool/brightxchange/repositories/products/MotherboardDAO.java
+++ server/src/main/java/com/codecool/brightxchange/repositories/products/MotherboardDAO.java
@@ -1,9 +1,55 @@
 package com.codecool.brightxchange.repositories.products;
 
+import com.codecool.brightxchange.model.Supplier;
+import com.codecool.brightxchange.model.producers.ChipsetProducer;
+import com.codecool.brightxchange.model.producers.MotherboardProducer;
 import com.codecool.brightxchange.model.products.Motherboard;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.awt.*;
 
 @Repository
 public interface MotherboardDAO extends JpaRepository<Motherboard, Long> {
+    @Modifying
+    @Query("update Motherboard m set" +
+            " m.price = :price," +
+            " m.quantity = :quantity," +
+            " m.chipset = :chipset," +
+            " m.numberOfM2 = :numberOfM2," +
+            " m.numberOfSATA3 = :numberOfSATA3" +
+            " where m.id= :id ")
+    void update(@Param("id") long id,
+                @Param("price") float price,
+                @Param("quantity") Integer quantity,
+                @Param("chipset") String chipset,
+                @Param("numberOfM2") Integer numberOfM2,
+                @Param("numberOfSATA3") Integer numberOfSATA3);
+
+
+    @Modifying
+    @Query("update Motherboard m set m.chipsetProducer = :chipsetProducer where m.id = :id")
+    void updateChipsetProducer(@Param("id") long id,
+                               @Param("chipsetProducer")ChipsetProducer chipsetProducer);
+
+
+
+    @Modifying
+    @Query("update Motherboard m set m.motherboardProducer = :motherboardProducer where m.id = :id")
+    void updateMotherboardProducer(@Param("id") long id,
+                                   @Param("motherboardProducer")MotherboardProducer motherboardProducer
+    );
+
+    @Modifying
+    @Query("update Motherboard m set m.supplier = :supplier where m.id = :id")
+    void updateSupplier(@Param("id") long id,
+                                   @Param("supplier") Supplier supplier
+    );
+
+
 }
+
+
--- server/src/main/java/com/codecool/brightxchange/service/products/MotherboardService.java
+++ server/src/main/java/com/codecool/brightxchange/service/products/MotherboardService.java
@@ -1,8 +1,16 @@
 package com.codecool.brightxchange.service.products;
 
+import com.codecool.brightxchange.model.Supplier;
+import com.codecool.brightxchange.model.producers.ChipsetProducer;
+import com.codecool.brightxchange.model.producers.MotherboardProducer;
+import com.codecool.brightxchange.model.products.GraphicsCard;
+import com.codecool.brightxchange.model.products.Motherboard;
 import com.codecool.brightxchange.repositories.products.MotherboardDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service("MotherboardService")
 public class MotherboardService {
@@ -11,4 +19,45 @@ public class MotherboardService {
     public MotherboardService(MotherboardDAO motherboardDAO) {
         this.motherboardDAO = motherboardDAO;
     }
+
+    public List<Motherboard> getAll(){
+        return motherboardDAO.findAll();
+    }
+
+    public Optional<Motherboard> getById(long id){
+        return motherboardDAO.findById(id);
+    }
+
+    public void addMotherboard(Motherboard tempMotherboard){
+        motherboardDAO.save(tempMotherboard);
+    }
+
+    public void updateMotherboard(long id,
+                                  float price,
+                                  Integer quantity,
+                                  String chipset,
+                                  Integer numberOfM2,
+                                  Integer numberOfSATA3){
+        motherboardDAO.update(id,
+                price,
+                quantity,
+                chipset,
+                numberOfM2,
+                numberOfSATA3
+        );
+
+    }
+    public void updateSupplier(long id, Supplier supplier){
+        motherboardDAO.updateSupplier(id, supplier);
+    }
+
+    public void updateMotherboardProducer(long id, MotherboardProducer motherboardProducer){
+        motherboardDAO.updateMotherboardProducer(id, motherboardProducer);
+    }
+
+    public void updateChipsetProducer(long id, ChipsetProducer chipsetProducer){
+        motherboardDAO.updateChipsetProducer(id, chipsetProducer);
+    }
+
+
 }
