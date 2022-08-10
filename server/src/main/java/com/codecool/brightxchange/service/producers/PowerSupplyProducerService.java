a3ec58b01c6c88af308aaa84d098a2a80cbeff7a
Loredana27 <loredanastefania1227@gmail.com>
2022-08-10 17:06:07 +0300

    update PowerSupplyProducerDAO and PowerSupplyProducerService

--- server/src/main/java/com/codecool/brightxchange/service/producers/PowerSupplyProducerService.java
+++ server/src/main/java/com/codecool/brightxchange/service/producers/PowerSupplyProducerService.java
@@ -1,8 +1,13 @@
 package com.codecool.brightxchange.service.producers;
 
+import com.codecool.brightxchange.model.producers.MonitorProducer;
+import com.codecool.brightxchange.model.producers.PowerSupplyProducer;
 import com.codecool.brightxchange.repositories.producers.PowerSupplyProducerDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service("PowerSupplyProducerService")
 public class PowerSupplyProducerService {
@@ -10,5 +15,21 @@ public class PowerSupplyProducerService {
 
     public PowerSupplyProducerService(PowerSupplyProducerDAO powerSupplyProducerDAO) {
         this.powerSupplyProducerDAO = powerSupplyProducerDAO;
+    }
+
+    public List<PowerSupplyProducer> getAll(){
+        return powerSupplyProducerDAO.findAll();
+    }
+
+    public Optional<PowerSupplyProducer> getById(long id){
+        return powerSupplyProducerDAO.findById(id);
+    }
+
+    public void addPowerSupplyProducer(PowerSupplyProducer tempPowerSupplyProducer){
+        powerSupplyProducerDAO.save(tempPowerSupplyProducer);
+    }
+
+    public void updatePowerSupplyProducer(long id, String name){
+        powerSupplyProducerDAO.update(id, name);
     }
 }
