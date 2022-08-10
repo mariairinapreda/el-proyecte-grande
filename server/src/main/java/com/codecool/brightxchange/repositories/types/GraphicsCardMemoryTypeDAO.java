11564311926ed1fb2dd196c8de9e7f36137ebe31
mariairinapreda <mariairina.predaeu@gmail.com>
2022-08-10 16:42:24 +0300

    implemented GraphicsCardMemoryTypeDAO

--- server/src/main/java/com/codecool/brightxchange/repositories/types/GraphicsCardMemoryTypeDAO.java
+++ server/src/main/java/com/codecool/brightxchange/repositories/types/GraphicsCardMemoryTypeDAO.java
@@ -2,8 +2,17 @@ package com.codecool.brightxchange.repositories.types;
 
 import com.codecool.brightxchange.model.productTypes.GraphicsCardMemoryType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface GraphicsCardMemoryTypeDAO extends JpaRepository<GraphicsCardMemoryType, Long> {
+    @Modifying
+    @Query("update GraphicsCardMemoryType g set  g.name= :name  where g.id= :id ")
+    void update(@Param("id") long id, @Param("name")String name);
+
+
+
 }
