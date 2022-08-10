651edfbb3feec71a149a7777c9ba44a5cdd14e40
Loredana27 <loredanastefania1227@gmail.com>
2022-08-10 17:06:34 +0300

    update ProcessorProducerDAO and ProcessorProducerService

--- server/src/main/java/com/codecool/brightxchange/service/producers/ProcessorProducerService.java
+++ server/src/main/java/com/codecool/brightxchange/service/producers/ProcessorProducerService.java
@@ -1,8 +1,13 @@
 package com.codecool.brightxchange.service.producers;
 
+import com.codecool.brightxchange.model.producers.PowerSupplyProducer;
+import com.codecool.brightxchange.model.producers.ProcessorProducer;
 import com.codecool.brightxchange.repositories.producers.ProcessorProducerDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service("ProcessorProducerService")
 public class ProcessorProducerService {
@@ -10,5 +15,21 @@ public class ProcessorProducerService {
 
     public ProcessorProducerService(ProcessorProducerDAO processorProducerDAO) {
         this.processorProducerDAO = processorProducerDAO;
+    }
+
+    public List<ProcessorProducer> getAll(){
+        return processorProducerDAO.findAll();
+    }
+
+    public Optional<ProcessorProducer> getById(long id){
+        return processorProducerDAO.findById(id);
+    }
+
+    public void addProcessorProducer(ProcessorProducer tempProcessorProducer){
+        processorProducerDAO.save(tempProcessorProducer);
+    }
+
+    public void updateProcessorProducer(long id, String name){
+        processorProducerDAO.update(id, name);
     }
 }
