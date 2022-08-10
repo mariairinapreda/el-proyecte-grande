a584bd9e67368082fa479d8dc199a1cf39df0394
Loredana27 <loredanastefania1227@gmail.com>
2022-08-10 17:03:34 +0300

    update MonitorDAO and MonitorService

--- server/src/main/java/com/codecool/brightxchange/repositories/products/MonitorDAO.java
+++ server/src/main/java/com/codecool/brightxchange/repositories/products/MonitorDAO.java
@@ -1,9 +1,57 @@
 package com.codecool.brightxchange.repositories.products;
 
+import com.codecool.brightxchange.model.Supplier;
+import com.codecool.brightxchange.model.producers.MonitorProducer;
+import com.codecool.brightxchange.model.productRelated.VideoBackPanel;
 import com.codecool.brightxchange.model.products.Monitor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MonitorDAO extends JpaRepository<Monitor, Long> {
+    @Modifying
+    @Query("update Monitor m set " +
+            "m.price = :price," +
+            " m.quantity = :quantity," +
+            " m.power = :power," +
+            " m.diagonal = :diagonal," +
+            " m.dimensions = :dimensions," +
+            " m.refreshRate= :refreshRate," +
+            " m.responseTime = :responseTime," +
+            " m.weight = :responseTime" +
+            " where m.id= :id ")
+    void update(@Param("id") long id,
+                @Param("price") float price,
+                @Param("quantity") Integer quantity,
+                @Param("power") Integer power,
+                @Param("diagonal") Integer diagonal,
+                @Param("dimensions") Integer dimensions,
+                @Param("refreshRate") Integer refreshRate,
+                @Param("responseTime") Integer responseTime,
+                @Param("weight") Integer weight
+                );
+
+    @Modifying
+    @Query("update Monitor m set m.monitorProducer = :monitorProducer where m.id =:id")
+    void updateProducer(@Param("id") long id,
+                        @Param("monitorProducer") MonitorProducer monitorProducer);
+
+    @Modifying
+    @Query("update Monitor m set m.backPanel = :backPanel where m.id =:id")
+    void updateVideoBackPanel(@Param("id") long id,
+                        @Param("monitorProducer") VideoBackPanel backPanel);
+
+    @Modifying
+    @Query("update Monitor m set m.supplier = :supplier where m.id =:id")
+    void updateSupplier(@Param("id") long id,
+                        @Param("supplier") Supplier supplier);
+
+
+
+
+
+
 }
--- server/src/main/java/com/codecool/brightxchange/service/products/MonitorService.java
+++ server/src/main/java/com/codecool/brightxchange/service/products/MonitorService.java
@@ -1,8 +1,15 @@
 package com.codecool.brightxchange.service.products;
 
+import com.codecool.brightxchange.model.Supplier;
+import com.codecool.brightxchange.model.producers.MonitorProducer;
+import com.codecool.brightxchange.model.productRelated.VideoBackPanel;
+import com.codecool.brightxchange.model.products.Monitor;
 import com.codecool.brightxchange.repositories.products.MonitorDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service("MonitorService")
 public class MonitorService {
@@ -11,4 +18,51 @@ public class MonitorService {
     public MonitorService(MonitorDAO monitorDAO) {
         this.monitorDAO = monitorDAO;
     }
+
+    public List<Monitor> getAll(){
+        return monitorDAO.findAll();
+    }
+
+    public Optional<Monitor> getById(long id){
+        return monitorDAO.findById(id);
+    }
+
+    public void addMonitor(Monitor tempMonitor){
+        monitorDAO.save(tempMonitor);
+    }
+
+    public void updateMonitor(long id,
+                              float price,
+                              Integer quantity,
+                              Integer power,
+                              Integer diagonal,
+                              Integer dimensions,
+                              Integer refreshRate,
+                              Integer responseTime,
+                              Integer weight){
+        monitorDAO.update(id,
+                price,
+                quantity,
+                power,
+                diagonal,
+                dimensions,
+                refreshRate,
+                responseTime,
+                weight);
+    }
+
+
+    public void updateMonitorProducer(long id, MonitorProducer monitorProducer){
+        monitorDAO.updateProducer(id, monitorProducer);
+    }
+
+    public  void updateVideoBackPanel(long id, VideoBackPanel backPanel){
+        monitorDAO.updateVideoBackPanel(id, backPanel);
+    }
+
+    public void updateSupplier(long id, Supplier supplier){
+        monitorDAO.updateSupplier(id, supplier);
+    }
+
+
 }
