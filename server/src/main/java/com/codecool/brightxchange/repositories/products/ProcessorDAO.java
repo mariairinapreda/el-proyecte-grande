92f7254d4b697e7485bccedd7437560db7fedff9
Loredana27 <loredanastefania1227@gmail.com>
2022-08-10 17:08:17 +0300

    update ProcessorDAO and ProcessorService

--- server/src/main/java/com/codecool/brightxchange/repositories/products/ProcessorDAO.java
+++ server/src/main/java/com/codecool/brightxchange/repositories/products/ProcessorDAO.java
@@ -1,9 +1,51 @@
 package com.codecool.brightxchange.repositories.products;
 
+import com.codecool.brightxchange.model.Supplier;
+import com.codecool.brightxchange.model.producers.ProcessorProducer;
+import com.codecool.brightxchange.model.products.Desktop;
 import com.codecool.brightxchange.model.products.Processor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProcessorDAO extends JpaRepository<Processor, Long> {
+    @Modifying
+    @Query("update Processor p set" +
+            " p.price = :price," +
+            " p.quantity = :quantity," +
+            " p.core = :core," +
+            " p.frequency = :frequency," +
+            " p.frequencyTurboMax = :frequencyTurboMax," +
+            " p.numberOfCores = :numberOfCores," +
+            " p.productionFactory = :productionFactory," +
+            " p.socket = :socket," +
+            " p.threads = :socket" +
+            " where p.id= :id ")
+    void update(@Param("id") long id,
+                @Param("price") float price,
+                @Param("quantity") Integer quantity,
+                @Param("core") String core,
+                @Param("frequency") Integer frequency,
+                @Param("frequencyTurboMax") Integer frequencyTurboMax,
+                @Param("numberOfCores") Integer numberOfCores,
+                @Param("productionFactory") Integer productionFactory,
+                @Param("socket") Integer socket,
+                @Param("threads") Integer threads
+                );
+
+
+    @Modifying
+    @Query("update Processor p set p.supplier = :supplier where p.id = :id")
+    void updateSupplier(@Param("id") float id,
+                        @Param("supplier") Supplier supplier);
+
+    @Modifying
+    @Query("update Processor  p set p.producer = :producer where p.id = :id")
+    void updateProducer(@Param("id") float id,
+                        @Param("producer")ProcessorProducer producer);
+
+
 }
--- server/src/main/java/com/codecool/brightxchange/service/products/ProcessorService.java
+++ server/src/main/java/com/codecool/brightxchange/service/products/ProcessorService.java
@@ -1,8 +1,14 @@
 package com.codecool.brightxchange.service.products;
 
+import com.codecool.brightxchange.model.Supplier;
+import com.codecool.brightxchange.model.producers.ProcessorProducer;
+import com.codecool.brightxchange.model.products.Processor;
 import com.codecool.brightxchange.repositories.products.ProcessorDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service("ProcessorService")
 public class ProcessorService {
@@ -11,4 +17,48 @@ public class ProcessorService {
     public ProcessorService(ProcessorDAO processorDAO) {
         this.processorDAO = processorDAO;
     }
+
+    public List<Processor> getAll(){
+        return processorDAO.findAll();
+    }
+
+    public Optional<Processor> getById(long id){
+        return processorDAO.findById(id);
+    }
+
+    public void addProcessor(Processor tempProcessor){
+        processorDAO.save(tempProcessor);
+    }
+
+    public void updateProcessor(long id,
+                                float price,
+                                Integer quantity,
+                                String core,
+                                Integer frequency,
+                                Integer frequencyTurboMax,
+                                Integer numberOfCores,
+                                Integer productionFactory,
+                                Integer socket,
+                                Integer threads){
+        processorDAO.update(id,
+                price,
+                quantity,
+                core,
+                frequency,
+                frequencyTurboMax,
+                numberOfCores,
+                productionFactory,
+                socket,
+                threads );
+    }
+
+    public void updateSupplier(long id, Supplier supplier){
+        processorDAO.updateSupplier(id, supplier);
+    }
+
+    public void updateProducer(long id, ProcessorProducer producer){
+        processorDAO.updateProducer(id, producer);
+    }
+
+
 }
