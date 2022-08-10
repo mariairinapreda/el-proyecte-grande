dcf4eb348b99468167d8c1fdc709f5cb3ca3a0ce
Loredana27 <loredanastefania1227@gmail.com>
2022-08-10 17:09:55 +0300

    update SsdDAO and SsdService

--- server/src/main/java/com/codecool/brightxchange/repositories/products/SsdDAO.java
+++ server/src/main/java/com/codecool/brightxchange/repositories/products/SsdDAO.java
@@ -1,9 +1,32 @@
 package com.codecool.brightxchange.repositories.products;
 
+import com.codecool.brightxchange.model.Supplier;
 import com.codecool.brightxchange.model.products.SSD;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SsdDAO extends JpaRepository<SSD, Long> {
+
+    @Modifying
+    @Query("update SSD s set" +
+            " s.quantity =:quantity," +
+            " s.readSpeed = :readSpeed," +
+            " s.writeSpeed = :writeSpeed, s.price =:price" +
+            " where s.id= :id ")
+    void update(@Param("id") long id,
+                @Param("quantity") Integer quantity,
+                @Param("readSpeed") Integer readSpeed,
+                @Param("writeSpeed") Integer writeSpeed,
+                @Param("price") float price);
+
+
+    @Modifying
+    @Query("update SSD s set s.supplier = :supplier where s.id = : id")
+    void updateSupplier(@Param("id") float id,
+                        @Param("supplier")Supplier supplier);
+
 }
--- server/src/main/java/com/codecool/brightxchange/service/products/SsdService.java
+++ server/src/main/java/com/codecool/brightxchange/service/products/SsdService.java
@@ -1,8 +1,13 @@
 package com.codecool.brightxchange.service.products;
 
+import com.codecool.brightxchange.model.Supplier;
+import com.codecool.brightxchange.model.products.SSD;
 import com.codecool.brightxchange.repositories.products.SsdDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service("SsdService")
 public class SsdService {
@@ -12,4 +17,26 @@ public class SsdService {
     public SsdService(SsdDAO ssdDAO) {
         this.ssdDAO = ssdDAO;
     }
+
+    public List<SSD> getAll(){
+        return ssdDAO.findAll();
+    }
+
+    public Optional<SSD> getById(long id){
+        return ssdDAO.findById(id);
+    }
+
+    public void addSSD(SSD tempSSD){
+        ssdDAO.save(tempSSD);
+    }
+
+    public void updateSSD(long id, Integer quantity, Integer readSpeed, Integer writeSpeed, float price){
+        ssdDAO.update(id, quantity, readSpeed, writeSpeed, price);
+    }
+
+    public void updateSupplier(long id, Supplier supplier){
+        ssdDAO.updateSupplier(id, supplier);
+    }
+
+
 }
