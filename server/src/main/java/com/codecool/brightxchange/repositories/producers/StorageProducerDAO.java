c13c671c9f8bdec369b35550b34b6dd2b99d9a88
Loredana27 <loredanastefania1227@gmail.com>
2022-08-10 17:07:25 +0300

    update StorageProducerDAO and StorageProducerService

--- server/src/main/java/com/codecool/brightxchange/repositories/producers/StorageProducerDAO.java
+++ server/src/main/java/com/codecool/brightxchange/repositories/producers/StorageProducerDAO.java
@@ -2,8 +2,15 @@ package com.codecool.brightxchange.repositories.producers;
 
 import com.codecool.brightxchange.model.producers.StorageProducer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StorageProducerDAO extends JpaRepository<StorageProducer, Long> {
+    @Modifying
+    @Query("update StorageProducer  sp set sp.name = :name where sp.id = :id")
+    void update(@Param("id") float id,
+                @Param("name") String name);
 }
--- server/src/main/java/com/codecool/brightxchange/service/producers/StorageProducerService.java
+++ server/src/main/java/com/codecool/brightxchange/service/producers/StorageProducerService.java
@@ -1,8 +1,13 @@
 package com.codecool.brightxchange.service.producers;
 
+import com.codecool.brightxchange.model.producers.PowerSupplyProducer;
+import com.codecool.brightxchange.model.producers.StorageProducer;
 import com.codecool.brightxchange.repositories.producers.StorageProducerDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service("StorageProducerService")
 public class StorageProducerService {
@@ -10,5 +15,21 @@ public class StorageProducerService {
 
     public StorageProducerService(StorageProducerDAO storageProducerDAO) {
         this.storageProducerDAO = storageProducerDAO;
+    }
+
+    public List<StorageProducer> getAll(){
+        return storageProducerDAO.findAll();
+    }
+
+    public Optional<StorageProducer> getById(long id){
+        return storageProducerDAO.findById(id);
+    }
+
+    public void addStorageProducer(StorageProducer tempStorageProducer){
+        storageProducerDAO.save(tempStorageProducer);
+    }
+
+    public void updateStorageProducer(long id, String name){
+        storageProducerDAO.update(id, name);
     }
 }
