0fe40ecb10720bcfca88de42f6f874b5ababc1a8
Loredana27 <loredanastefania1227@gmail.com>
2022-08-10 17:01:18 +0300

    update ChipsetProducerDAO and ChipsetProducerService

--- server/src/main/java/com/codecool/brightxchange/service/producers/ChipsetProducerService.java
+++ server/src/main/java/com/codecool/brightxchange/service/producers/ChipsetProducerService.java
@@ -1,8 +1,12 @@
 package com.codecool.brightxchange.service.producers;
 
+import com.codecool.brightxchange.model.producers.ChipsetProducer;
 import com.codecool.brightxchange.repositories.producers.ChipsetProducerDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service("ChipsetProducerService")
 public class ChipsetProducerService {
@@ -10,5 +14,21 @@ public class ChipsetProducerService {
 
     public ChipsetProducerService(ChipsetProducerDAO chipsetProducerDAO) {
         this.chipsetProducerDAO = chipsetProducerDAO;
+    }
+
+    public List<ChipsetProducer> getAll(){
+        return chipsetProducerDAO.findAll();
+    }
+
+    public Optional<ChipsetProducer> getById(long id){
+        return chipsetProducerDAO.findById(id);
+    }
+
+    public void addChipsetProducer(ChipsetProducer tempChipsetProducer){
+        chipsetProducerDAO.save(tempChipsetProducer);
+    }
+
+    public void updateChipsetProducer(long id, String name){
+        chipsetProducerDAO.update(id, name);
     }
 }
