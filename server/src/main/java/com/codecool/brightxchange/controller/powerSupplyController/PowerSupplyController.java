5518722e1e1e6f25c2b9de42485d6f7d58201382
mariairinapreda <mariairina.predaeu@gmail.com>
2022-08-11 13:35:00 +0300

    implemented PowerSupplyController

--- server/src/main/java/com/codecool/brightxchange/controller/powerSupplyController/PowerSupplyController.java
+++ server/src/main/java/com/codecool/brightxchange/controller/powerSupplyController/PowerSupplyController.java
@@ -1,7 +1,11 @@
 package com.codecool.brightxchange.controller.powerSupplyController;
 
 
+import com.codecool.brightxchange.model.Supplier;
+import com.codecool.brightxchange.model.producers.PowerSupplyProducer;
+import com.codecool.brightxchange.model.productTypes.PowerSupplyType;
 import com.codecool.brightxchange.model.products.PowerSupply;
+import com.codecool.brightxchange.service.products.PowerSupplyService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,16 +14,48 @@ import java.util.List;
 @RestController
 @RequestMapping("/power-supply")
 public class PowerSupplyController {
+    private final PowerSupplyService powerSupplyService;
+
+
+    public PowerSupplyController(PowerSupplyService powerSupplyService) {
+        this.powerSupplyService = powerSupplyService;
+    }
 
     @GetMapping
     public List<PowerSupply> getAll(){
-        return null;
+        return powerSupplyService.getAll();
     }
 
     @GetMapping("{id}")
     public PowerSupply getById(@PathVariable("id") Long id){
-        return null;
+        return powerSupplyService.getById(id).get();
     }
 
+    @PostMapping
+    public void addPowerSupply(@RequestBody PowerSupply powerSupply){
+        powerSupplyService.addPowerSupply(powerSupply);
+    }
+
+    @PutMapping("{id}")
+    public void updatePowerSupply(@PathVariable("id")long id, @RequestBody PowerSupply powerSupply){
+        powerSupplyService.updatePowerSupply(id, powerSupply.getPower(),
+                powerSupply.getQuantity(),
+                powerSupply.getCertification(),
+                powerSupply.getQuantity());
+    }
+
+    @PutMapping("{id}")
+    public void updateSupplier(@PathVariable("id")long id,@RequestBody Supplier supplier){
+        powerSupplyService.updateSupplier(id, supplier);
+    }
+    @PutMapping("{id}")
+    public void updateType(@PathVariable("id")long id, @RequestBody PowerSupplyType powerSupplyType){
+        powerSupplyService.updatePowerSupplyType(id, powerSupplyType);
+    }
+
+    @PutMapping("{id}")
+    public void updatePowerSupplyProducer(@PathVariable("id")long id, @RequestBody PowerSupplyProducer powerSupplyProducer){
+        powerSupplyService.updatePowerSupplyProducer(id, powerSupplyProducer);
+    }
 
 }
