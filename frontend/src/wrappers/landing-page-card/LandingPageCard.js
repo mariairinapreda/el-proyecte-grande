fc5e9131ba0657233bd618b0cabe60213b804852
Sidor Andrei <sidor.marian.andrei3001@gmail.com>
2022-08-11 10:42:41 +0300

    adjust design for LandingPageCard

--- frontend/src/wrappers/landing-page-card/LandingPageCard.js
+++ frontend/src/wrappers/landing-page-card/LandingPageCard.js
@@ -1,20 +1,23 @@
 import classes from "./LandingPageCard.module.scss";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
-const LandingPageCard = ({url, imageUrl, name}) => {
-    return <div className={classes.categoryCard}>
-        <Link to={url}>
-            <img
-                className={classes.backgroundImage}
-                src={imageUrl}
-                alt={name + " Image"}
-            />
-            <div className={classes.content}>
-                <p className={classes.title}>{name}</p>
-                <p className={classes.subtitle}>Vezi produse</p>
-            </div>
-        </Link>
+const LandingPageCard = ({ url, imageUrl, name }) => {
+  // console.log(`url("${imageUrl}")`);
+  return (
+    <div className={classes.categoryCard}>
+      <Link to={url}>
+        <div
+          className={classes.backgroundImage}
+          style={{ backgroundImage: `url(${imageUrl})!important` }}
+        >
+          <div className={classes.content}>
+            <p className={classes.title}>{name}</p>
+            <p className={classes.subtitle}>Vezi produse</p>
+          </div>
+        </div>
+      </Link>
     </div>
-}
+  );
+};
 
 export default LandingPageCard;
