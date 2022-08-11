d0965b271f460c4b16ac0f21c772f3cf230b6718
mariairinapreda <mariairina.predaeu@gmail.com>
2022-08-11 13:32:53 +0300

    implemented ChipsetProducerController

--- server/src/main/java/com/codecool/brightxchange/controller/motherboardController/ChipsetProducerController.java
+++ server/src/main/java/com/codecool/brightxchange/controller/motherboardController/ChipsetProducerController.java
@@ -1,6 +1,7 @@
 package com.codecool.brightxchange.controller.motherboardController;
 
 import com.codecool.brightxchange.model.producers.ChipsetProducer;
+import com.codecool.brightxchange.service.producers.ChipsetProducerService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -9,15 +10,31 @@ import java.util.List;
 @RestController
 @RequestMapping("/chipset-producer")
 public class ChipsetProducerController {
+    private final ChipsetProducerService chipsetProducerService;
+
+
+    public ChipsetProducerController(ChipsetProducerService chipsetProducerService) {
+        this.chipsetProducerService = chipsetProducerService;
+    }
 
     @GetMapping
     public List<ChipsetProducer> getAll(){
-        return null;
+        return chipsetProducerService.getAll();
     }
 
     @GetMapping("{id}")
     public ChipsetProducer getById(@PathVariable("id") Long id){
-        return null;
+        return chipsetProducerService.getById(id).get();
     }
 
+
+    @PostMapping
+    public void addChipsetProducer(@RequestBody ChipsetProducer chipsetProducer){
+        chipsetProducerService.addChipsetProducer(chipsetProducer);
+    }
+
+    @PutMapping("{id}")
+    public void updateChipsetProducer(@PathVariable("id")long id, @RequestBody ChipsetProducer chipsetProducer){
+        chipsetProducerService.updateChipsetProducer(id, chipsetProducer.getName());
+    }
 }
