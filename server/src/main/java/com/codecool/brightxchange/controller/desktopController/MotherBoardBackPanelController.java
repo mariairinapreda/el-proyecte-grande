bb65b2f37dda140907c394a9ec90a01fe971246c
Loredana27 <loredanastefania1227@gmail.com>
2022-08-11 13:26:38 +0300

    implement MotherBoardBackPanelController

--- server/src/main/java/com/codecool/brightxchange/controller/desktopController/MotherBoardBackPanelController.java
+++ server/src/main/java/com/codecool/brightxchange/controller/desktopController/MotherBoardBackPanelController.java
@@ -2,6 +2,7 @@ package com.codecool.brightxchange.controller.desktopController;
 
 
 import com.codecool.brightxchange.model.productRelated.MotherBoardBackPanel;
+import com.codecool.brightxchange.service.productRelated.MotherBoardBackPanelService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,14 +11,29 @@ import java.util.List;
 @RestController
 @RequestMapping("/mother-board-back-panel")
 public class MotherBoardBackPanelController {
+    private MotherBoardBackPanelService motherBoardBackPanelService;
+
+    public MotherBoardBackPanelController(MotherBoardBackPanelService motherBoardBackPanelService) {
+        this.motherBoardBackPanelService = motherBoardBackPanelService;
+    }
 
     @GetMapping
     public List<MotherBoardBackPanel> getAll(){
-        return null;
+        return motherBoardBackPanelService.getAll();
     }
 
     @GetMapping("{id}")
     public MotherBoardBackPanel getById(@PathVariable("id") Long id){
-        return null;
+        return motherBoardBackPanelService.getById(id).orElse(null);
+    }
+
+    @PostMapping
+    public void saveNewMotherboardBackPanel(@RequestBody MotherBoardBackPanel motherBoardBackPanel){
+        motherBoardBackPanelService.saveMotherBoardBackPanel(motherBoardBackPanel);
+    }
+
+    @PutMapping("{id}")
+    public void updateMotherBoardBackPanel(@PathVariable("id") Long id, @RequestBody MotherBoardBackPanel motherBoardBackPanel){
+        motherBoardBackPanelService.updateMotherBoardBackPanel(id, motherBoardBackPanel);
     }
 }
