2f6791a7cc3314ac7f4c661ca380581b3f87cc67
Loredana27 <loredanastefania1227@gmail.com>
2022-08-11 13:25:28 +0300

    implement GraphicsCardMemoryTypeController

--- server/src/main/java/com/codecool/brightxchange/controller/graphicsCardController/GraphicsCardMemoryTypeController.java
+++ server/src/main/java/com/codecool/brightxchange/controller/graphicsCardController/GraphicsCardMemoryTypeController.java
@@ -1,6 +1,7 @@
 package com.codecool.brightxchange.controller.graphicsCardController;
 
 import com.codecool.brightxchange.model.productTypes.GraphicsCardMemoryType;
+import com.codecool.brightxchange.service.types.GraphicsCardMemoryTypeService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,14 +11,26 @@ import java.util.List;
 @RequestMapping("/graphics-card-memory-type")
 public class GraphicsCardMemoryTypeController {
 
+    private GraphicsCardMemoryTypeService graphicsCardMemoryTypeService;
+
     @GetMapping
     public List<GraphicsCardMemoryType> getAll(){
-        return null;
+        return graphicsCardMemoryTypeService.getAll();
     }
 
     @GetMapping("{id}")
     public GraphicsCardMemoryType getById(@PathVariable("id") Long id){
-        return null;
+        return graphicsCardMemoryTypeService.getById(id).orElse(null);
+    }
+
+    @PostMapping
+    public void saveNewGraphicsCardMemoryType(@RequestBody GraphicsCardMemoryType graphicsCardMemoryType){
+        graphicsCardMemoryTypeService.addGCMemoryType(graphicsCardMemoryType);
+    }
+
+    @PutMapping("{id}")
+    public void updateGraphicsCardMemoryType(@PathVariable("id")Long id, @RequestBody GraphicsCardMemoryType graphicsCardMemoryType){
+        graphicsCardMemoryTypeService.updateGraphicsCardMemoryType(id, graphicsCardMemoryType);
     }
 
 }
