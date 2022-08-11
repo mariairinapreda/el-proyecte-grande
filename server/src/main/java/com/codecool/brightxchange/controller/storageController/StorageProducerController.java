c6dcf426c857164233900f4de2382fdf1d9d3b94
mariairinapreda <mariairina.predaeu@gmail.com>
2022-08-11 13:39:24 +0300

    implemented StorageProducerController

--- server/src/main/java/com/codecool/brightxchange/controller/storageController/StorageProducerController.java
+++ server/src/main/java/com/codecool/brightxchange/controller/storageController/StorageProducerController.java
@@ -1,6 +1,7 @@
 package com.codecool.brightxchange.controller.storageController;
 
 import com.codecool.brightxchange.model.producers.StorageProducer;
+import com.codecool.brightxchange.service.producers.StorageProducerService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -9,14 +10,30 @@ import java.util.List;
 @RestController
 @RequestMapping("/storage-producer")
 public class StorageProducerController {
+    private final StorageProducerService storageProducerService;
+
+    public StorageProducerController(StorageProducerService storageProducerService) {
+        this.storageProducerService = storageProducerService;
+    }
+
     @GetMapping
     public List<StorageProducer> getAll(){
-        return null;
+        return storageProducerService.getAll();
     }
 
     @GetMapping("{id}")
     public StorageProducer getById(@PathVariable("id") Long id){
-        return null;
+        return storageProducerService.getById(id).get();
+    }
+
+    @PostMapping
+    public void addStorageProducer(@RequestBody StorageProducer storageProducer){
+        storageProducerService.addStorageProducer(storageProducer);
+    }
+
+    @PutMapping("{id}")
+    public void updateStorageProducer(@PathVariable("id")long id, @RequestBody StorageProducer storageProducer){
+        storageProducerService.updateStorageProducer(id, storageProducer.getName());
     }
 
 
