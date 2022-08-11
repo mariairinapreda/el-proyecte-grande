33400d3f5402a756f33392bda9a49899e326e191
Loredana27 <loredanastefania1227@gmail.com>
2022-08-11 13:23:48 +0300

    implement DisplayController

--- server/src/main/java/com/codecool/brightxchange/controller/displayController/DisplayController.java
+++ server/src/main/java/com/codecool/brightxchange/controller/displayController/DisplayController.java
@@ -1,6 +1,7 @@
 package com.codecool.brightxchange.controller.displayController;
 
 import com.codecool.brightxchange.model.productRelated.Display;
+import com.codecool.brightxchange.service.products.DisplayService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,14 +11,31 @@ import java.util.List;
 @RequestMapping("/display")
 public class DisplayController {
 
+    private DisplayService displayService;
+
+
+    public DisplayController(DisplayService displayService) {
+        this.displayService = displayService;
+    }
+
     @GetMapping
     public List<Display> getAll(){
-        return null;
+        return displayService.getAll();
     }
 
     @GetMapping("{id}")
     public Display getById(@PathVariable("id") Long id){
-        return null;
+        return displayService.getDisplayById(id).orElse(null);
+    }
+
+    @PostMapping
+    public void saveNewDisplay(@RequestBody Display display){
+        displayService.addDisplay(display);
+    }
+
+    @PutMapping("{id}")
+    public void updateDisplay(@PathVariable("id") Long id, @RequestBody Display display){
+        displayService.updateDisplay(id, display);
     }
 
 
