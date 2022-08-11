9b673bf27801bccacd85eb5a66885a2a6edd3fe4
Loredana27 <loredanastefania1227@gmail.com>
2022-08-11 13:25:13 +0300

    implement GraphicsCardFamilyController

--- server/src/main/java/com/codecool/brightxchange/controller/graphicsCardController/GraphicsCardFamilyController.java
+++ server/src/main/java/com/codecool/brightxchange/controller/graphicsCardController/GraphicsCardFamilyController.java
@@ -1,6 +1,7 @@
 package com.codecool.brightxchange.controller.graphicsCardController;
 
 import com.codecool.brightxchange.model.productRelated.GraphicsCardFamily;
+import com.codecool.brightxchange.service.productRelated.GraphicsCardFamilyService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -9,15 +10,34 @@ import java.util.List;
 @RestController
 @RequestMapping("/graphics-card-family")
 public class GraphicsCardFamilyController {
+
+    private GraphicsCardFamilyService graphicsCardFamilyService;
+
+    public GraphicsCardFamilyController(GraphicsCardFamilyService graphicsCardFamilyService) {
+        this.graphicsCardFamilyService = graphicsCardFamilyService;
+    }
+
     @GetMapping
     public List<GraphicsCardFamily> getAll(){
-        return null;
+        return graphicsCardFamilyService.getAll();
     }
 
     @GetMapping("{id}")
     public GraphicsCardFamily getById(@PathVariable("id") Long id){
-        return null;
+        return graphicsCardFamilyService.getGraphicsCardFamilyById(id).orElse(null);
     }
+
+    @PostMapping
+    public void saveNewGraphicsCardFamily(@RequestBody GraphicsCardFamily graphicsCardFamily){
+        graphicsCardFamilyService.saveGraphicsCardFamily(graphicsCardFamily);
+    }
+
+    @PutMapping("{id}")
+    public void updateGraphicsCardFamily(@PathVariable("id") Long id, @RequestBody GraphicsCardFamily graphicsCardFamily){
+        graphicsCardFamilyService.updateGraphicCardFamily(id, graphicsCardFamily);
+    }
+
+
 
 
 }
