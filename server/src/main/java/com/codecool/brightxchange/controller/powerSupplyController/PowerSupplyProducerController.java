9e2a48e29e89a9f1ad83ea609aec6cd2aec037de
mariairinapreda <mariairina.predaeu@gmail.com>
2022-08-11 13:35:40 +0300

    implemented PowerSupplyProducerController

--- server/src/main/java/com/codecool/brightxchange/controller/powerSupplyController/PowerSupplyProducerController.java
+++ server/src/main/java/com/codecool/brightxchange/controller/powerSupplyController/PowerSupplyProducerController.java
@@ -1,6 +1,7 @@
 package com.codecool.brightxchange.controller.powerSupplyController;
 
 import com.codecool.brightxchange.model.producers.PowerSupplyProducer;
+import com.codecool.brightxchange.service.producers.PowerSupplyProducerService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -9,15 +10,32 @@ import java.util.List;
 @RestController
 @RequestMapping("/power-supply-producer")
 public class PowerSupplyProducerController {
+    private final PowerSupplyProducerService producerService;
 
+
+    public PowerSupplyProducerController(PowerSupplyProducerService producerService) {
+        this.producerService = producerService;
+    }
 
     @GetMapping
     public List<PowerSupplyProducer> getAll(){
-        return null;
+        return producerService.getAll();
     }
 
     @GetMapping("{id}")
     public PowerSupplyProducer getById(@PathVariable("id") Long id){
-        return null;
+        return producerService.getById(id).get();
     }
+
+    @PostMapping
+    public void addProducer(@RequestBody PowerSupplyProducer powerSupplyProducer){
+        producerService.addPowerSupplyProducer(powerSupplyProducer);
+    }
+
+    @PutMapping("{id}")
+    public void updatePowerSupplyProducer(@PathVariable("id")long id, @RequestBody PowerSupplyProducer producer){
+        producerService.updatePowerSupplyProducer(id, producer.getName());
+    }
+
+
 }
