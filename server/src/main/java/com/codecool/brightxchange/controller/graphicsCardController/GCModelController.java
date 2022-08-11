642714a613f61d98e0c1535fb9034a1a023803c0
Loredana27 <loredanastefania1227@gmail.com>
2022-08-11 13:24:33 +0300

    implement GCModelController

--- server/src/main/java/com/codecool/brightxchange/controller/graphicsCardController/GCModelController.java
+++ server/src/main/java/com/codecool/brightxchange/controller/graphicsCardController/GCModelController.java
@@ -1,6 +1,7 @@
 package com.codecool.brightxchange.controller.graphicsCardController;
 
 import com.codecool.brightxchange.model.productRelated.GCModel;
+import com.codecool.brightxchange.service.productRelated.GCModelService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,14 +11,31 @@ import java.util.List;
 @RequestMapping("/graphics-card-model")
 public class GCModelController {
 
+    private GCModelService gcModelService;
+
+    public GCModelController(GCModelService gcModelService) {
+        this.gcModelService = gcModelService;
+    }
+
     @GetMapping
     public List<GCModel> getAll(){
-        return null;
+        return gcModelService.getAll();
     }
 
     @GetMapping("{id}")
     public GCModel getById(@PathVariable("id") Long id){
-        return null;
+        return gcModelService.getGCModelById(id).orElse(null);
+    }
+
+
+    @PostMapping
+    public void saveNewGCModel(@RequestBody GCModel gcModel){
+        gcModelService.addGraphicCardModel(gcModel);
+    }
+
+    @PostMapping("{id}")
+    public void updateGCModel(@PathVariable("id") long id, @RequestBody GCModel gcModel){
+        gcModelService.updateGCModel(id, gcModel);
     }
 
 }
