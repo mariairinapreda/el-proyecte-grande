830af886fee1377f9e98e83787e01f1f024956ea
Loredana27 <loredanastefania1227@gmail.com>
2022-08-11 13:23:13 +0300

    implement CoolerController

--- server/src/main/java/com/codecool/brightxchange/controller/coolerController/CoolerController.java
+++ server/src/main/java/com/codecool/brightxchange/controller/coolerController/CoolerController.java
@@ -1,7 +1,9 @@
 package com.codecool.brightxchange.controller.coolerController;
 
 
+import com.codecool.brightxchange.model.Supplier;
 import com.codecool.brightxchange.model.products.Cooler;
+import com.codecool.brightxchange.service.products.CoolerService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,14 +13,35 @@ import java.util.List;
 @RequestMapping("/cooler")
 public class CoolerController {
 
+    private CoolerService coolerService;
+
+    public CoolerController(CoolerService coolerService) {
+        this.coolerService = coolerService;
+    }
+
     @GetMapping
     public List<Cooler> getAll(){
-        return null;
+        return coolerService.getAll();
     }
 
     @GetMapping("{id}")
     public Cooler getById(@PathVariable("id") Long id){
-        return null;
+        return coolerService.getById(id).orElse(null);
+    }
+
+    @PostMapping
+    public void addCooler(@RequestBody Cooler tempCooler){
+        coolerService.addCooler(tempCooler);
+    }
+
+
+    @PutMapping("{id}")
+    public void updateCooler(@PathVariable("id") long id, @RequestBody Cooler updatedCooler){coolerService.updateCooler(id,updatedCooler);}
+
+
+    @PutMapping("{id}")
+    public void updateCoolerSupplier(@PathVariable Long id, @RequestBody Supplier supplier){
+        coolerService.updateSupplier(id, supplier);
     }
 
 }
