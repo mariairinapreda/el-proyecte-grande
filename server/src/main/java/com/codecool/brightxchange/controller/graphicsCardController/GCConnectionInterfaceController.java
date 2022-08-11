56334457f9893fa31895e94c0d04c2d272ac2283
Loredana27 <loredanastefania1227@gmail.com>
2022-08-11 13:24:22 +0300

    implement GCConnectionInterfaceController

--- server/src/main/java/com/codecool/brightxchange/controller/graphicsCardController/GCConnectionInterfaceController.java
+++ server/src/main/java/com/codecool/brightxchange/controller/graphicsCardController/GCConnectionInterfaceController.java
@@ -1,6 +1,7 @@
 package com.codecool.brightxchange.controller.graphicsCardController;
 
 import com.codecool.brightxchange.model.productRelated.GCConnectionInterface;
+import com.codecool.brightxchange.service.productRelated.GCConnectionInterfaceService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,15 +11,27 @@ import java.util.List;
 @RequestMapping("/gc-connection-interface")
 public class GCConnectionInterfaceController {
 
+    private GCConnectionInterfaceService gcConnectionInterfaceService;
+
 
     @GetMapping
     public List<GCConnectionInterface> getAll(){
-        return null;
+        return gcConnectionInterfaceService.getAll();
     }
 
     @GetMapping("{id}")
     public GCConnectionInterface getById(@PathVariable("id") Long id){
-        return null;
+        return gcConnectionInterfaceService.getById(id).orElse(null);
+    }
+
+    @PostMapping
+    public void saveNewGCConnectionInterface(@RequestBody GCConnectionInterface gcConnectionInterface){
+        gcConnectionInterfaceService.addConnectionInterface(gcConnectionInterface);
+    }
+
+    @PutMapping("{id}")
+    public void updateGCConnectionInterface(@PathVariable("id") Long id, @RequestBody GCConnectionInterface gcConnectionInterface){
+        gcConnectionInterfaceService.updateConnectionInterface(id, gcConnectionInterface);
     }
 
 }
