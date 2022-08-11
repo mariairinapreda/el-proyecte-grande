1ab1629d1583c2686ff5df1ce4cfe86fd11e897f
Sidor Andrei <sidor.marian.andrei3001@gmail.com>
2022-08-11 11:40:21 +0300

    add HDD Card

--- frontend/src/pages/products/HddPage.js
+++ frontend/src/pages/products/HddPage.js
@@ -1,31 +1,57 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
+import ProductsContainer from "../../wrappers/products-container/ProductsContainer";
+import Card from "../../wrappers/card/Card";
+import HddDetails from "../../wrappers/card/details/hdd-details/HddDetails";
 
 const HddPage = () => {
-  const [hdds, setHdds] = useState([{
-    price : 135,
-    currency : {
-      currency: "RON",
+  const [hdds, setHdds] = useState([
+    {
+      price: 135,
+      currency: {
+        currency: "RON",
+      },
+      quantity: 5,
+      supplier: {
+        name: "Bright Xchange",
+      },
+      rotationSpeed: 350,
+      connectionInterface: "SATA-III",
+      capacity: 5,
+      unit: "TB",
+      storageProducer: {
+        name: "Samsung",
+      },
+      imageUrl:
+        "https://brightxchangeprojectbucket.s3.eu-central-1.amazonaws.com/cat/hard-disk-uri.jpg",
     },
-    quantity : 5,
-    supplier: {
-      name: "Bright Xchange",
-    },
-    rotationSpeed: 350,
-    connectionInterface:  "",
-    capacity: 5,
-    unit: "TB",
-    storageProducer: {
-      name: ""
-    }
-  }]);
+  ]);
 
   useEffect(() => {
-    fetch("http://localhost:8888/hdd", {method:"GET"})
-        .then(response => response.json())
-        .then(re => setHdds(re))
-  },[])
+    fetch("http://localhost:8888/hdd", { method: "GET" })
+      .then((response) => response.json())
+      .then((re) => setHdds(re));
+  }, []);
 
-  return <></>;
+  return (
+    <ProductsContainer>
+      {hdds.map((e, index) => (
+        <Card
+          key={`hdd_${e.storageProducer}_${index}`}
+          title={`Hard-Disk ${e.storageProducer.name} ${e.capacity} ${e.unit}`}
+          imageUrl={e.imageUrl}
+          price={e.price}
+          currency={e.currency.currency}
+          details={
+            <HddDetails
+              connectionInterface={e.connectionInterface}
+              capacity={e.capacity}
+              unit={e.unit}
+            />
+          }
+        />
+      ))}
+    </ProductsContainer>
+  );
 };
 
 export default HddPage;
