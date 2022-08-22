63e7503ec64395ebaedbc0b1460abf9cbdd0956a
mariairinapreda <mariairina.predaeu@gmail.com>
2022-08-22 11:19:51 +0300

    implemented add for CategorySpecService

--- server2/src/main/java/com/codecool/brightxchange/service/CategorySpecService.java
+++ server2/src/main/java/com/codecool/brightxchange/service/CategorySpecService.java
@@ -1,7 +1,10 @@
 package com.codecool.brightxchange.service;
 
+import com.codecool.brightxchange.model.CategorySpec;
 import com.codecool.brightxchange.repositories.CategorySpecRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CategorySpecService {
@@ -11,4 +14,13 @@ public class CategorySpecService {
     public CategorySpecService(CategorySpecRepository repository) {
         this.repository = repository;
     }
+
+    public void addCategorySpec(CategorySpec categorySpec){
+        repository.save(categorySpec);
+    }
+    public void addAllCategorySpec(List<CategorySpec> categorySpecList){
+        repository.saveAll(categorySpecList);
+    }
+
+
 }
