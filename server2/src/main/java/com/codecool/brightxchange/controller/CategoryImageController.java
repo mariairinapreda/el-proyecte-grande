a339b8398a410e4edc968fce2e3ec915f9f1e04a
Loredana27 <loredanastefania1227@gmail.com>
2022-08-22 11:18:19 +0300

    implement CategoryImageController and CategoryImageService

--- server2/src/main/java/com/codecool/brightxchange/controller/CategoryImageController.java
+++ server2/src/main/java/com/codecool/brightxchange/controller/CategoryImageController.java
@@ -2,10 +2,14 @@ package com.codecool.brightxchange.controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @CrossOrigin
 @RestController
 @RequestMapping("category-images")
 public class CategoryImageController {
+
+    public void imageUploader(@RequestParam("image")MultipartFile image){}
 }
