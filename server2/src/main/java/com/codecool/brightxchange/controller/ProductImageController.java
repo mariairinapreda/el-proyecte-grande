32cb420744cb85460c8e19f3900fcda5af6ceae1
Loredana27 <loredanastefania1227@gmail.com>
2022-08-22 11:42:36 +0300

    implement ProductImageController

--- server2/src/main/java/com/codecool/brightxchange/controller/ProductImageController.java
+++ server2/src/main/java/com/codecool/brightxchange/controller/ProductImageController.java
@@ -2,10 +2,14 @@ package com.codecool.brightxchange.controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @CrossOrigin
 @RestController
 @RequestMapping("product-images")
 public class ProductImageController {
+
+    public void imageUploader(@RequestParam("image") MultipartFile image){}
 }
