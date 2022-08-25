70327e8371dff0c10cb3d17d5d3a6c43efd15e00
SidorAndrei <sidor.marian.andrei3001@gmail.com>
2022-08-25 03:38:52 +0300

    add cors

--- server2/src/main/java/com/codecool/brightxchange/springSecurity/security/ApplicationSecurityConfig.java
+++ server2/src/main/java/com/codecool/brightxchange/springSecurity/security/ApplicationSecurityConfig.java
@@ -37,8 +37,9 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
         http
+                .cors()
+                .and()
                 .csrf().disable()
 //                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
 //                .and()
