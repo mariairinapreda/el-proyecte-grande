b8b85bbc8954bc42289f3f610d2e9e4b7f33381d
mariairinapreda <mariairina.predaeu@gmail.com>
2022-08-31 10:21:38 +0300

    implemented CartItem

---
+++ server2/src/main/java/com/codecool/brightxchange/model/CartItem.java
@@ -0,0 +1,39 @@
+package com.codecool.brightxchange.model;
+
+import com.codecool.brightxchange.springSecurity.entity.Client;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+import org.hibernate.annotations.Entity;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class CartItem {
+    @Id
+    @SequenceGenerator(
+            name = "cart_item_sequence",
+            sequenceName = "cart_item_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "cart_item_sequence"
+    )
+    @JsonProperty("id")
+    private Long id;
+    @ManyToOne
+    @JsonIgnore
+    private Client client;
+    @ManyToOne(cascade = {CascadeType.ALL})
+    private Product product;
+    private Integer quantity;
+
+
+
+}
