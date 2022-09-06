7cf1749c858b1347829400a5989c762f0cf6eda8
mariairinapreda <mariairina.predaeu@gmail.com>
2022-09-06 13:22:25 +0300

    refactored Controllers

---
+++ server2/src/main/java/com/codecool/brightxchange/controller/categorySpecController/CategorySpecController.java
@@ -0,0 +1,11 @@
+package com.codecool.brightxchange.controller.categorySpecController;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+@RequestMapping("category-specs")
+public class CategorySpecController {
+}
---
+++ server2/src/main/java/com/codecool/brightxchange/controller/productController/ProductController.java
@@ -0,0 +1,23 @@
+package com.codecool.brightxchange.controller.productController;
+
+import com.codecool.brightxchange.model.Product;
+import com.codecool.brightxchange.service.ProductService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+@RequestMapping("products")
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    public void addProduct(Product product) {
+        productService.addProductService(product);
+    }
+}
