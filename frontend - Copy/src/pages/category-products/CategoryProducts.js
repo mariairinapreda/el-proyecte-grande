d6e61cd28ebd6da1587977e2189eb780e07f5df6
Sidor Andrei <sidor.marian.andrei3001@gmail.com>
2022-09-08 11:35:59 +0300

    work on products by category

--- frontend - Copy/src/pages/category-products/CategoryProducts.js
+++ frontend - Copy/src/pages/category-products/CategoryProducts.js
@@ -1,12 +1,31 @@
-import {useParams} from "react-router-dom";
+import { useParams } from "react-router-dom";
+import Navigation from "../../components/navigation/Navigation";
+import ProductsContainer from "../../wrappers/products-container/ProductsContainer";
+import { useEffect, useState } from "react";
+import Card from "../../wrappers/card/Card";
+import axios from "axios";
+import { BASE_PATH } from "../../atoms/STORE";
 
 const CategoryProducts = () => {
-  const {categoryName} = useParams();
-  console.log(categoryName)
+  const { categoryName } = useParams();
+  const [products, setProducts] = useState([]);
+  console.log(categoryName);
+  useEffect(() => {
+    axios
+      .get(`${BASE_PATH}/products/${categoryName}`)
+      .then((r) => setProducts(r.data));
+  }, [categoryName]);
+  return (
+    <>
+      <Navigation />
+      <ProductsContainer>
+        <h1>{categoryName}</h1>
+        {products.map((p) => (
+          <Card />
+        ))}
+      </ProductsContainer>
+    </>
+  );
+};
 
-    return (
-        <>THIS IS THE CATEGORY: {categoryName}</>
-    )
-}
-
-export default CategoryProducts
+export default CategoryProducts;
