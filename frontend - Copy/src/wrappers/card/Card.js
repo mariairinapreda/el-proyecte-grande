34c1ef854cfe66d1c7ef44438e89cc48d4893a98
Sidor Andrei <sidor.marian.andrei3001@gmail.com>
2022-09-19 13:18:30 +0300

    work on cart

--- frontend - Copy/src/wrappers/card/Card.js
+++ frontend - Copy/src/wrappers/card/Card.js
@@ -1,25 +1,31 @@
 import classes from "./Card.module.scss";
 
-const Card = ({title, imageUrl, details, price}) => {
-    return (
-        <div className={classes.customCard}>
-            <img className={classes.customCardImage} src={imageUrl} alt="None"/>
-            <div className={classes.customCardHeader}>
-                <p className={classes.customCardTitle}>{title}</p>
-            </div>
-            <div style={{color: "white"}}>{details}</div>
-            <div className={classes.customCardBody}>
-                <div className={classes.customCardText}>
-                    <p>
-                        {price} {"RON"}
-                    </p>
-                </div>
-                <div className={classes.customCardText}>
-                    <button className="btn btn-success add-product">Adauga in cos</button>
-                </div>
-            </div>
+const Card = ({ title, imageUrl, details, price, onAdd, productId }) => {
+  return (
+    <div className={classes.customCard}>
+      <img className={classes.customCardImage} src={imageUrl} alt="None" />
+      <div className={classes.customCardHeader}>
+        <p className={classes.customCardTitle}>{title}</p>
+      </div>
+      <div style={{ color: "white" }}>{details}</div>
+      <div className={classes.customCardBody}>
+        <div className={classes.customCardText}>
+          <p>
+            {price} {"RON"}
+          </p>
         </div>
-    );
+        <div className={classes.customCardText}>
+          <button
+            id={productId}
+            onClick={onAdd}
+            className="btn btn-success add-product"
+          >
+            Adauga in cos
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
